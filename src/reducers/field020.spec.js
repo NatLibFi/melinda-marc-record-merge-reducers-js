4d4e18dd4df1a5fa7441ba0ad124e80bf11b1912
ssilvonen <46997466+ssilvonen@users.noreply.github.com>
2020-12-03 18:20:18 +0200

    Working on 020, created custom merge functions

--- src/reducers/field020.spec.js
+++ src/reducers/field020.spec.js
@@ -4,12 +4,12 @@ import path from 'path';
 import {MarcRecord} from '@natlibfi/marc-record';
 import createReducer from './field020';
 import fixturesFactory, {READERS} from '@natlibfi/fixura';
-import createDebugLogger from 'debug';
+//import createDebugLogger from 'debug';
 
 MarcRecord.setValidationOptions({subfieldValues: false});
 
 describe('reducers/field020', () => {
-  const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
+  //const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
   const {expect} = chai;
   const fixturesPath = path.join(__dirname, '..', '..', 'test-fixtures', 'reducers', 'field020');
 
@@ -19,10 +19,17 @@ describe('reducers/field020', () => {
       const base = new MarcRecord(getFixture('base.json'));
       const source = new MarcRecord(getFixture('source.json'));
       const tagPattern = new RegExp(getFixture({components: ['tagPattern.txt'], reader: READERS.TEXT}), 'u');
-      debug(`tagPattern: ${tagPattern}`);
+      const dropSubfields = getDropSubfields();
       const expectedRecord = getFixture('merged.json');
-      const mergedRecord = createReducer({tagPattern})(base, source);
+      const mergedRecord = createReducer({tagPattern, dropSubfields})(base, source);
       expect(mergedRecord.toObject()).to.eql(expectedRecord);
+
+      // Check whether dropSubfields.json exists and if it does, return its contents. If not, do nothing.
+      function getDropSubfields() {
+        const subfieldsToDrop = getFixture({components: ['dropSubfields.json'], reader: READERS.JSON});
+        return subfieldsToDrop ? subfieldsToDrop : undefined;
+      }
+
     });
   });
 });
