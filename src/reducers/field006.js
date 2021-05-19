34bbecbdc1e91991846c30242c02fe13758387eb
nvolk <nicholas.volk@helsinki.fi>
2021-05-19 10:46:16 +0300

    Refactor code. (Slightly optimize and get rid of lint complaints.)

--- src/reducers/field006.js
+++ src/reducers/field006.js
@@ -4,21 +4,25 @@ import {checkIdenticalness, copyNonIdenticalFields} from './utils.js';
 // Test 02: If Leader 000/06 is 'o' or 'p' in source, copy 006 from source to base as new field (2x)
 // Test 03: If Leader 000/06 is something else, do nothing
 
+// NV: Moved these of the arrow function
+const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
+const regexp006 = /^006$/u;
+
 export default () => (base, source) => {
-  const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
-  const baseFields = base.get(/^006$/u);
-  const sourceFields = source.get(/^006$/u);
-
-  const nonIdenticalFields = checkIdenticalness(baseFields, sourceFields);
-
-  if (nonIdenticalFields.length === 0) {
-    debug(`Identical fields in source and base`);
+  // No action required, always keep base (do this first as we save, uh, like 0.01 ms by not doing unnecessary stuff):
+  if (source.leader[6] !== 'o' && source.leader[6] !== 'p') {
+    debug('Keeping base field 006');
     return base;
   }
 
-  if (source.leader[6] === 'o' || source.leader[6] === 'p') {
-    return copyNonIdenticalFields(base, nonIdenticalFields);
+  const baseFields = base.get(regexp006);
+  const sourceFields = source.get(regexp006);
+  const nonIdenticalFields = checkIdenticalness(baseFields, sourceFields);
+
+  if (nonIdenticalFields.length === 0) {
+    debug('Identical fields in source and base');
+    return base;
   }
-  debug(`Keeping base field 006`);
-  return base;
+
+  return copyNonIdenticalFields(base, nonIdenticalFields);
 };
