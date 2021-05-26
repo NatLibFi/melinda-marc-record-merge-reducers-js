82e336e7439f4e293a8b6ddf71bcc5e8444a33c9
nvolk <nicholas.volk@helsinki.fi>
2021-05-26 23:09:21 +0300

    Refactor code.

--- src/reducers/field020.js
+++ src/reducers/field020.js
@@ -21,23 +21,16 @@ const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 const repCodes = ['q', 'z', '8'];
 const nonRepCodes = ['a', 'c', '6'];
 
-function mergeField020(base, baseField, sourceField) {
-  debug(`Working on field 020`);
-  // First check whether the values of identifying subfields are equal
-  const idCodes = ['a'];
+// If there are subfields to drop, define them first
+const dropCodes = ['c'];
 
-  // Case 1: If all identifying subfield values are not equal, the entire source field is copied to base as a new field
-  if (compareAllSubfields(baseField, sourceField, idCodes) === false) {
-    base.insertField(sourceField);
-    idCodes.forEach(code => debug(`Subfield (${code}) not matching, source field copied as new field to base`));
-    return base; // Base returned in case 1
-  }
+const idCodes = ['a'];
+
+function mergeField020Step2(base, baseField, sourceField) {
+
 
   // Case 2: If identifying subfield values are equal, continue with the merge process
   idCodes.forEach(code => debug(`Matching subfield (${code}) found in source and base, continuing with merge`));
-
-  // If there are subfields to drop, define them first
-  const dropCodes = ['c'];
 
   // Copy other subfields from source field to base field
   // For non-repeatable subfields, the value existing in base is preferred
@@ -54,6 +47,23 @@ function mergeField020(base, baseField, sourceField) {
   return makeNewBaseField(base, baseField, sortedSubfields);
 }
 
+function mergeField020(base, baseField, sourceField) {
+  debug(`Working on field 020`);
+  // First check whether the values of identifying subfields are equal
+
+  // Case 1: If all identifying subfield values are not equal, the entire source field is copied to base as a new field
+  // NV: the name "compareAllSubfields" gives no clue about what the function returns. Change name?
+  // Also "All" is misleading,
+  if (compareAllSubfields(baseField, sourceField, idCodes) === false) {
+    base.insertField(sourceField);
+    idCodes.forEach(code => debug(`Subfield (${code}) not matching, source field copied as new field to base`));
+    return base; // Base returned in case 1
+  }
+
+  return mergeField020Step2(base, baseField, sourceField);
+
+}
+
 export default () => (base, source) => {
   const baseFields = base.get(fieldTag); // Get array of base fields
   const sourceFields = source.get(fieldTag); // Get array of source fields
@@ -66,8 +76,8 @@ export default () => (base, source) => {
   }
 
 
-  if (sourceFields.every(sourceField => baseFields.some(baseField => mergeField020(base, baseField, sourceField, repCodes, nonRepCodes)))) {
-    // No filtering needed here since mergeField020 does it in a customized way
-    return base;
-  }
+  sourceFields.every(sourceField => baseFields.some(baseField => mergeField020(base, baseField, sourceField)));
+  // No filtering needed here since mergeField020 does it in a customized way
+  return base;
+
 };
