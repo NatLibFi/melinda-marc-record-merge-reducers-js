e8e92d397977d24ef2f2bd3a708f5b5a6c3366bf
nvolk <nicholas.volk@helsinki.fi>
2021-06-10 23:55:43 +0300

    New file. Might be renamed lated. Generic rules for field merging.

---
+++ src/reducers/utils2.js
@@ -0,0 +1,341 @@
+import {MarcRecord} from '@natlibfi/marc-record';
+import createDebugLogger from 'debug';
+import {
+  // controlSubfieldsPermitMerge,
+  fieldHasSubfield,
+  fieldIsRepeatable,
+  fieldToString,
+  normalizeStringValue
+  //normalizeStringValue
+} from './utils.js';
+
+const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
+
+const mergeRestrictions = [
+  {'tag': '020', 'required': 'a', 'paired': '', 'key': ''},
+  // NB! 100, 110 and 111 may have title parts that are handled elsewhere
+  {'tag': '100', 'required': 'a', 'paired': 't', 'key': 'abcj'},
+  {'tag': '110', 'required': 'a', 'paired': 't', 'key': 'abcdgn'},
+  {'tag': '111', 'required': 'a', 'paired': 't', 'key': 'acdgn'},
+  // NB! 130 has no name part, key is used for title part
+  {'tag': '130', 'required': 'a', 'paired': '', 'key': 'adfhklmnoprsxvg'},
+  // NB! 700, 710 and 711 may have title parts that are handled elsewhere
+  {'tag': '700', 'required': 'a', 'paired': 't', 'key': 'abcj'},
+  {'tag': '710', 'required': 'a', 'paired': 't', 'key': 'abcdgn'},
+  {'tag': '711', 'required': 'a', 'paired': 't', 'key': 'acdgn'},
+  // NB! 730 has no name part, key is used for title part
+  {'tag': '730', 'required': 'a', 'paired': '', 'key': 'adfhklmnoprsxvg'}
+];
+
+function getUniqueKeyFields2(tag) {
+  const activeTags = mergeRestrictions.filter(entry => tag === entry.tag);
+  if (activeTags.length === 0) {
+    debug(`Warning\tNo key found for ${tag}`);
+    return '';
+  }
+  if (activeTags.length > 1) {
+    debug(`Warning\tMultiple keys (N=${activeTags.length}) found for ${tag}`);
+    return activeTags[0].key;
+  }
+
+  return activeTags[0].key;
+}
+
+function getUniqueKeyFields(field) {
+  const keys = getUniqueKeyFields2(field.tag);
+  debug(`Unique key for ${field.tag}: '${keys}'`);
+  // NB! We should add exceptions here, eg 710$a$t tekijänimekkeet...
+  /*
+    if ( field.tag === '100' && fieldHasSubfield(field, 't') ) {
+
+    }*/
+
+  return keys;
+}
+
+function uniqueKeyMatches(field1, field2, forcedKeyString = null) {
+  // NB! Assume that field1 and field2 have same relevant subfields.
+  // We might have 100 vs 700 fields. I haven't check whether their specs are identical.
+  const keySubfieldsAsString = forcedKeyString || getUniqueKeyFields(field1);
+  const subfieldArray = keySubfieldsAsString.split('');
+  return subfieldArray.every(subfieldCode => {
+    const subfields1 = field1.subfields.filter(subfield => subfield.code === subfieldCode);
+    const subfields2 = field2.subfields.filter(subfield => subfield.code === subfieldCode);
+    // Assume that at least 1 instance must exist and that all instances must match
+    if (subfields1.length !== subfields2.length) {
+      debug(`Unique key: subfield ${subfieldCode} issues...`);
+      return false;
+    }
+
+    return subfields1.every(sf => {
+      const normSubfieldValue = normalizeStringValue(sf.value);
+      return subfields2.some(sf2 => {
+        const normSubfieldValue2 = normalizeStringValue(sf2.value);
+        return normSubfieldValue === normSubfieldValue2;
+      });
+    });
+
+  });
+}
+
+function mergeGetPairedSubfieldCodes(tag) {
+  const activeTags = mergeRestrictions.filter(entry => tag === entry.tag);
+  if (activeTags.length === 0) {
+    debug(`Warning\tNo merge subfield rules found for ${tag}`);
+    return '';
+  }
+  if (activeTags.length > 1) {
+    debug(`Warning\tMultiple merge subfield rules (N=${activeTags.length}) found for ${tag}`);
+    return activeTags[0].required;
+  }
+
+  return activeTags[0].required;
+}
+
+function mergeGetRequiredSubfieldCodes(tag) {
+  const activeTags = mergeRestrictions.filter(entry => tag === entry.tag);
+  if (activeTags.length === 0) {
+    debug(`Warning\tNo merge subfield rules found for ${tag}`);
+    return '';
+  }
+  if (activeTags.length > 1) {
+    debug(`Warning\tMultiple merge subfield rules found for ${tag}`);
+    return activeTags[0].required;
+  }
+
+  return activeTags[0].required;
+}
+
+function areRequiredSubfieldsPresent(field) {
+  const subfieldString = mergeGetRequiredSubfieldCodes(field.tag);
+  const subfieldArray = subfieldString.split('');
+  return subfieldArray.every(sfcode => fieldHasSubfield(field, sfcode));
+}
+
+function arePairedSubfieldsInBalance(field1, field2) {
+  const subfieldString = mergeGetPairedSubfieldCodes(field1.tag);
+  const subfieldArray = subfieldString.split('');
+  return subfieldArray.every(sfcode => {
+    if (fieldHasSubfield(field1, sfcode)) {
+      // Return true if present in f1 and f1. Return false if present in f1 but missing in f2:
+      return fieldHasSubfield(field2, sfcode);
+    }
+    // subfield is missing in both
+    return !fieldHasSubfield(field2, sfcode);
+  });
+}
+
+function fieldToNamePart(field) {
+  const index = field.subfields.findIndex(currSubfield => currSubfield.code === 't');
+  const subsetField = {'tag': field.tag, 'ind1': field.ind1, 'ind2': field.ind2, subfields: field.subfields.filter((sf, i) => i < index)};
+  debug(`Name subset: ${fieldToString(subsetField)}`);
+  return subsetField;
+}
+
+function fieldToTitlePart(field) {
+  const index = field.subfields.findIndex(currSubfield => currSubfield.code === 't');
+  const subsetField = {'tag': field.tag, 'ind1': field.ind1, 'ind2': field.ind2, subfields: field.subfields.filter((sf, i) => i >= index)};
+  debug(`Title subset: ${fieldToString(subsetField)}`);
+  return subsetField;
+}
+
+function compareName(field1, field2) {
+  // 100$a$t: remove $t and everything after that
+  const subset1 = fieldToNamePart(field1);
+  const subset2 = fieldToNamePart(field2);
+  // compare the remaining subsets:
+  return uniqueKeyMatches(subset1, subset2);
+}
+
+function compareTitle(field1, field2) {
+  // 100$a$t: remove $t and everything after that
+  const subset1 = fieldToTitlePart(field1);
+  const subset2 = fieldToTitlePart(field2);
+  // "dfhklmnoprstxvg" is ok for 100, 110, 111, 700, 710 and 711. 130/730 is not handled here!
+  return uniqueKeyMatches(subset1, subset2, 'dfhklmnoprstxvg');
+}
+
+function compareNameAndTitle(field1, field2) {
+  // Both name and title parts exist:
+  if (fieldHasSubfield(field1, 't') && field1.tag in ['100', '110', '111', '700', '710', '711']) {
+    if (compareName(field1, field2) && compareTitle(field1, field2)) {
+      return true;
+    }
+    return false;
+  }
+
+  // Handle the field specific "unique key" (=set of fields that make the field unique
+  if (uniqueKeyMatches(field1, field2)) {
+    debug('Unique key matches. We are MERGABLE :-)');
+    return true;
+  }
+  return false;
+}
+
+export function mergablePair(field1, field2) {
+  // Indicators *must* be equal:
+  if (field1.ind1 !== field2.ind1 || field1.ind2 !== field2.ind2) {
+    debug('indicator check failed');
+    return false;
+  }
+
+  if (!controlSubfieldsPermitMerge(field1, field2)) {
+    debug('control subfield check failed');
+    return false;
+  }
+
+  // NB! field1.tag and field2.tag might differ. Therefore required subfields might theoretically differ as well. (1XX vs 7XX)
+  if (!areRequiredSubfieldsPresent(field1) || !areRequiredSubfieldsPresent(field2)) {
+    debug('required subfield presence check failed.');
+    return false;
+  }
+
+  // NB! field1.tag and field2.tag might differ. Therefore required subfields may differ as well.
+  if (!arePairedSubfieldsInBalance(field1, field2)) {
+    // Eg. require that both fields either have or have not X00$t:
+    debug('required subfield pair check failed.');
+    return false;
+  }
+
+  return compareNameAndTitle(field1, field2);
+}
+
+function controlSubfield1PermitsMerge(field1, field2) {
+  if (!fieldHasSubfield(field1, '1', null) && !fieldHasSubfield(field2, '1', null)) {
+    return true;
+  }
+  // Same result, but log:
+  debug(`NB: controlSubfield1PermitsMerge() not implemented yet. Always succeeds!`);
+  return true;
+}
+
+function controlSubfield3PermitsMerge(field1, field2) {
+  // Check OK if neither one has $3.
+  // Check fails if one field has $3 and the other one does not
+  if (!fieldHasSubfield(field1, '3')) {
+    return !fieldHasSubfield(field2, '3');
+  }
+  if (!fieldHasSubfield(field2, '3')) {
+    return false;
+  }
+  // Compare $3 subfields. If everything matches, OK, else FAIL:
+  const sf3Field1 = field1.subfields.filter(subfield => subfield.code === '3');
+  const sf3Field2 = field2.subfields.filter(subfield => subfield.code === '3');
+  return MarcRecord.isEqual(sf3Field1, sf3Field2);
+}
+
+function controlSubfield5PermitsMerge(field1, field2) {
+  // Check OK if neither one has $5.
+  // Check fails if one field has $5 and the other one does not
+  if (!fieldHasSubfield(field1, '5')) {
+    return !fieldHasSubfield(field2, '5');
+  }
+  if (!fieldHasSubfield(field2, '5')) {
+    return false;
+  }
+  // Strip $5 subfields. If everything else matches, OK, else FAIL:
+  const sf5lessField1 = field1.subfields.filter(subfield => subfield.code !== '5');
+  const sf5lessField2 = field2.subfields.filter(subfield => subfield.code !== '5');
+  return MarcRecord.isEqual(sf5lessField1, sf5lessField2);
+}
+
+function controlSubfield6PermitsMerge(field1, field2) {
+  if (!fieldHasSubfield(field1, '6') && !fieldHasSubfield(field2, '6')) {
+    return true;
+  }
+  debug('controlSubfield6PermitsMerge() not properly implemented.');
+  return false;
+}
+
+function controlSubfield8PermitsMerge(field1, field2) {
+  return !fieldHasSubfield(field1, '8') && !fieldHasSubfield(field2, '8');
+}
+
+function controlSubfield9PermitsMerge(field1, field2) {
+  if (!fieldHasSubfield(field1, '9') && !fieldHasSubfield(field2, '9')) {
+    return true;
+  }
+  const sf9lessField1 = field1.subfields.filter(subfield => subfield.code !== '9' || !(/(?:<KEEP>|<DROP>)/u).test(subfield.value));
+  const sf9lessField2 = field2.subfields.filter(subfield => subfield.code !== '9' || !(/(?:<KEEP>|<DROP>)/u).test(subfield.value));
+  return MarcRecord.isEqual(sf9lessField1, sf9lessField2);
+}
+
+export function controlSubfieldsPermitMerge(field1, field2) {
+  if (!controlSubfield0PermitsMerge(field1, field2)) {
+    debug(' csf0 failed');
+    return false;
+  }
+  if (!controlSubfield1PermitsMerge(field1, field2)) {
+    debug(' csf1 failed');
+    return false;
+  }
+  if (!controlSubfield3PermitsMerge(field1, field2)) {
+    debug(' csf3 failed');
+    return false;
+  }
+  if (!controlSubfield5PermitsMerge(field1, field2)) {
+    debug(' csf5 failed');
+    return false;
+  }
+  if (!controlSubfield6PermitsMerge(field1, field2)) {
+    debug(' csf6 failed');
+    return false;
+  }
+  if (!controlSubfield8PermitsMerge(field1, field2)) {
+    debug(' csf8 failed');
+    return false;
+  }
+  if (!controlSubfield9PermitsMerge(field1, field2)) {
+    debug(' csf9 failed');
+    return false;
+  }
+
+  return true;
+}
+
+function controlSubfield0PermitsMerge(field1, field2) {
+  if (!fieldHasSubfield(field1, '0', null) || !fieldHasSubfield(field2, '0', null)) {
+    return true;
+  }
+  return field1.subfields.every(sf => {
+    if (sf.code !== '0') {
+      return true;
+    }
+    // NB! Here we assume that value have been normalized beforehand.
+    // Eg. (isni) 0000 1234 5678 0000 vs https://isni.org/isni/0000123456780000
+    // Eg. FIN11 vs FI-ASTERI-N vs kanton uri
+
+    debug(`Compare ‡0 '${sf.value}' with '${fieldToString(field2)}'.`);
+    if (fieldHasSubfield(field2, field1.code, field1.value)) {
+      return true;
+    }
+
+    return prefixIsOK(sf, field2);
+
+    function getPrefix(currSubfield) {
+      if (currSubfield.value.match(/^\([^)]+\)[0-9]+$/u)) {
+        return currSubfield.value.substr(0, currSubfield.value.indexOf(')') + 1);
+      }
+      if (currSubfield.value.match(/^https?:/u)) {
+        return currSubfield.value.substr(0, currSubfield.value.lastindexOf('/') + 1);
+      }
+      return null;
+    }
+
+    function prefixIsOK(currSubfield, otherField) {
+      const prefix = getPrefix(currSubfield);
+      if (prefix === null) {
+        return false;
+      }
+      // Look for same prefix + different identifier
+      const hits = otherField.subfields.filter(sf2 => sf2.code === '0' && currSubfield.value !== sf2.value && sf2.value.indexOf(prefix) === 0);
+      if (hits.length > 0) {
+        debug(`Subfield ‡0 check FAILED: ‡0 '${currSubfield.value}' vs ‡0 '${hits[0].value}'.`);
+        return false;
+      }
+      debug(`Subfield ‡0 check OK: ${prefix} not found on ${fieldToString(otherField)}`);
+      return true;
+    }
+
+  });
+}
