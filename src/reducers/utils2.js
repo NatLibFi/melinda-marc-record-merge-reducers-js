fc608ffe0172da92f7dd34129217507ad9d25cae
nvolk <nicholas.volk@helsinki.fi>
2021-06-12 23:26:08 +0300

    Make the control subfield related code more generic

--- src/reducers/utils2.js
+++ src/reducers/utils2.js
@@ -125,9 +125,17 @@ function arePairedSubfieldsInBalance(field1, field2) {
   });
 }
 
+function namePartThreshold(field) {
+  const t = field.subfields.findIndex(currSubfield => currSubfield.code === 't');
+  const u = field.subfields.findIndex(currSubfield => currSubfield.code === 'u');
+  if ( t == -1 ) { return u; }
+  if ( u == -1 ) { return t; }
+  return ( t > u ? u : t );
+}
+
 function fieldToNamePart(field) {
-  const index = field.subfields.findIndex(currSubfield => currSubfield.code === 't');
-  const subsetField = {'tag': field.tag, 'ind1': field.ind1, 'ind2': field.ind2, subfields: field.subfields.filter((sf, i) => i < index)};
+  const index = namePartThreshold(field);
+  const subsetField = {'tag': field.tag, 'ind1': field.ind1, 'ind2': field.ind2, subfields: field.subfields.filter((sf, i) => i < index || index == -1 )};
   debug(`Name subset: ${fieldToString(subsetField)}`);
   return subsetField;
 }
@@ -200,13 +208,19 @@ export function mergablePair(field1, field2) {
   return compareNameAndTitle(field1, field2);
 }
 
-function controlSubfield1PermitsMerge(field1, field2) {
-  if (!fieldHasSubfield(field1, '1', null) && !fieldHasSubfield(field2, '1', null)) {
-    return true;
+function subfieldsAreEqual(field1, field2, subfieldCode) {
+// Check OK if neither one has given subfield.
+  // Check fails if one field has given subfield and the other one does not
+  if (!fieldHasSubfield(field1, subfieldCode)) {
+    return !fieldHasSubfield(field2, subfieldCode);
   }
-  // Same result, but log:
-  debug(`NB: controlSubfield1PermitsMerge() not implemented yet. Always succeeds!`);
-  return true;
+  if (!fieldHasSubfield(field2, subfieldCode)) {
+    return false;
+  }
+  // Compare $3 subfields. If everything matches, OK, else FAIL:
+  const sfSet1 = field1.subfields.filter(subfield => subfield.code === subfieldCode);
+  const sfSet2 = field2.subfields.filter(subfield => subfield.code === subfieldCode);
+  return MarcRecord.isEqual(sfSet1, sfSet2);
 }
 
 function controlSubfield3PermitsMerge(field1, field2) {
@@ -239,20 +253,26 @@ function controlSubfield5PermitsMerge(field1, field2) {
   return MarcRecord.isEqual(sf5lessField1, sf5lessField2);
 }
 
-function controlSubfield6PermitsMerge(field1, field2) {
-  if (!fieldHasSubfield(field1, '6') && !fieldHasSubfield(field2, '6')) {
+function subfieldsAreEmpty(field1, field2, subfieldCode) {
+  if (!fieldHasSubfield(field1, subfieldCode) && !fieldHasSubfield(field2, subfieldCode)) {
     return true;
+  }
+  return false;
+}
+
+
+
+function controlSubfield6PermitsMerge(field1, field2) {
+  if ( subfieldsAreEmpty(field1, field2, '6') ) {
+      return true;
   }
   debug('controlSubfield6PermitsMerge() not properly implemented.');
   return false;
 }
 
-function controlSubfield8PermitsMerge(field1, field2) {
-  return !fieldHasSubfield(field1, '8') && !fieldHasSubfield(field2, '8');
-}
 
 function controlSubfield9PermitsMerge(field1, field2) {
-  if (!fieldHasSubfield(field1, '9') && !fieldHasSubfield(field2, '9')) {
+  if ( subfieldsAreEmpty(field1, field2, '9') ) {
     return true;
   }
   const sf9lessField1 = field1.subfields.filter(subfield => subfield.code !== '9' || !(/(?:<KEEP>|<DROP>)/u).test(subfield.value));
@@ -260,28 +280,75 @@ function controlSubfield9PermitsMerge(field1, field2) {
   return MarcRecord.isEqual(sf9lessField1, sf9lessField2);
 }
 
+function getPrefix(currSubfield) {
+  if (currSubfield.value.match(/^\([^)]+\)[0-9]+$/u)) {
+    return currSubfield.value.substr(0, currSubfield.value.indexOf(')') + 1);
+  }
+  if (currSubfield.value.match(/^https?:/u)) {
+    return currSubfield.value.substr(0, currSubfield.value.lastindexOf('/') + 1);
+  }
+  return null;
+}
+
+function prefixIsOK(currSubfield, otherField, subfieldCode) {
+  const prefix = getPrefix(currSubfield);
+  if (prefix === null) {
+    return false;
+  }
+  // Look for same prefix + different identifier
+  const hits = otherField.subfields.filter(sf2 => sf2.code === subfieldCode && currSubfield.value !== sf2.value && sf2.value.indexOf(prefix) === 0);
+  if (hits.length > 0) {
+    debug(`Subfield ‡${subfieldCode} check FAILED: ‡${subfieldCode} '${currSubfield.value}' vs ‡${subfieldCode} '${hits[0].value}'.`);
+    return false;
+  }
+  debug(`Subfield ‡${subfieldCode} check OK: ${prefix} not found on ${fieldToString(otherField)}`);
+  return true;
+}
+
+
+
+function controlSubfieldContainingIdentifierPermitsMerge(field1, field2, subfieldCode) {
+  if (!fieldHasSubfield(field1, subfieldCode, null) || !fieldHasSubfield(field2, subfieldCode, null)) {
+    return true;
+  }
+  return field1.subfields.every(subfield => {
+    if (subfield.code !== subfieldCode) {
+      return true;
+    }
+    // NB! Here we assume that value have been normalized beforehand.
+    // Eg. (isni) 0000 1234 5678 0000 vs https://isni.org/isni/0000123456780000
+    // Eg. FIN11 vs FI-ASTERI-N vs kanton uri
+
+    debug(`Compare ‡${subfieldCode} '${subfield.value}' with '${fieldToString(field2)}'.`);
+    if (fieldHasSubfield(field2, field1.code, field1.value)) {
+      return true;
+    }
+
+    return prefixIsOK(subfield, field2, subfieldCode);
+
+  });
+}
+
+
+
+const controlSubfieldsContainingIdentifier = [ 'w', '0', '1' ];
 export function controlSubfieldsPermitMerge(field1, field2) {
-  if (!controlSubfield0PermitsMerge(field1, field2)) {
-    debug(' csf0 failed');
+  if (!controlSubfieldsContainingIdentifier.every(subfieldCode => { return controlSubfieldContainingIdentifierPermitsMerge(field1, field2, subfieldCode);}) ) {
+    debug(' control subfields with identifiers failed');
     return false;
   }
-  if (!controlSubfield1PermitsMerge(field1, field2)) {
-    debug(' csf1 failed');
+
+  if ( !subfieldsAreEqual(field1, field2, '3') || !subfieldsAreEqual(field1, field2, '5') ) {
+    debug(' similar control subfield failes');
     return false;
   }
-  if (!controlSubfield3PermitsMerge(field1, field2)) {
-    debug(' csf3 failed');
-    return false;
-  }
-  if (!controlSubfield5PermitsMerge(field1, field2)) {
-    debug(' csf5 failed');
-    return false;
-  }
+
   if (!controlSubfield6PermitsMerge(field1, field2)) {
     debug(' csf6 failed');
     return false;
   }
-  if (!controlSubfield8PermitsMerge(field1, field2)) {
+  // We don't handle $8 subfields here at all, as they affect multiple fields!
+  if (!subfieldsAreEmpty(field1, field2, '8')) {
     debug(' csf8 failed');
     return false;
   }
@@ -291,51 +358,4 @@ export function controlSubfieldsPermitMerge(field1, field2) {
   }
 
   return true;
-}
-
-function controlSubfield0PermitsMerge(field1, field2) {
-  if (!fieldHasSubfield(field1, '0', null) || !fieldHasSubfield(field2, '0', null)) {
-    return true;
-  }
-  return field1.subfields.every(sf => {
-    if (sf.code !== '0') {
-      return true;
-    }
-    // NB! Here we assume that value have been normalized beforehand.
-    // Eg. (isni) 0000 1234 5678 0000 vs https://isni.org/isni/0000123456780000
-    // Eg. FIN11 vs FI-ASTERI-N vs kanton uri
-
-    debug(`Compare ‡0 '${sf.value}' with '${fieldToString(field2)}'.`);
-    if (fieldHasSubfield(field2, field1.code, field1.value)) {
-      return true;
-    }
-
-    return prefixIsOK(sf, field2);
-
-    function getPrefix(currSubfield) {
-      if (currSubfield.value.match(/^\([^)]+\)[0-9]+$/u)) {
-        return currSubfield.value.substr(0, currSubfield.value.indexOf(')') + 1);
-      }
-      if (currSubfield.value.match(/^https?:/u)) {
-        return currSubfield.value.substr(0, currSubfield.value.lastindexOf('/') + 1);
-      }
-      return null;
-    }
-
-    function prefixIsOK(currSubfield, otherField) {
-      const prefix = getPrefix(currSubfield);
-      if (prefix === null) {
-        return false;
-      }
-      // Look for same prefix + different identifier
-      const hits = otherField.subfields.filter(sf2 => sf2.code === '0' && currSubfield.value !== sf2.value && sf2.value.indexOf(prefix) === 0);
-      if (hits.length > 0) {
-        debug(`Subfield ‡0 check FAILED: ‡0 '${currSubfield.value}' vs ‡0 '${hits[0].value}'.`);
-        return false;
-      }
-      debug(`Subfield ‡0 check OK: ${prefix} not found on ${fieldToString(otherField)}`);
-      return true;
-    }
-
-  });
 }
