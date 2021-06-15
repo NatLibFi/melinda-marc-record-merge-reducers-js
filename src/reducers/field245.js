90a3631fbc625ea7c2d374d48b4599055cbb3a89
nvolk <nicholas.volk@helsinki.fi>
2021-06-16 01:20:30 +0300

    Switch to new underlying merge logic.

--- src/reducers/field245.js
+++ src/reducers/field245.js
@@ -1,3 +1,4 @@
+import {mergeOrAddField} from './mergeField.js';
 import createDebugLogger from 'debug';
 
 import {
@@ -11,6 +12,14 @@ import {
 const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 const fieldTag = /^245$/u; // Tag in regexp format (for use in MarcRecord functions)
 
+export default () => (record, record2) => {
+  const candidateFields = record2.get(fieldTag); // Get array of source fields
+  candidateFields.forEach(candField => mergeOrAddField(record, candField));
+  return record;
+};
+
+
+/*
 export default () => (base, source) => {
   const baseFields = base.get(fieldTag); // Get array of base fields
   const sourceFields = source.get(fieldTag); // Get array of source fields
@@ -40,12 +49,11 @@ export default () => (base, source) => {
       debug(`Source 245 is longer, replacing base field with source field`);
       const newBaseField = JSON.parse(JSON.stringify(sourceField));
       // But indicator 2 is always taken from the base record
-      /* eslint-disable */
       newBaseField.ind2 = baseField.ind2;
       //base.removeField(baseField); // remove old baseField
       //base.insertField(newBaseField); // insert newBaseField
 
-      /* eslint-enable */
+
       return recordReplaceField(base, baseField, newBaseField);
       // return base;
     }
@@ -54,3 +62,4 @@ export default () => (base, source) => {
     return base;
   }
 };
+*/
