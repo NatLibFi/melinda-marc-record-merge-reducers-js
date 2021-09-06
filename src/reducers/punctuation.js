0706f3d824d5047bd6db1ee5dee549b7dfea0297
nvolk <nicholas.volk@helsinki.fi>
2021-09-06 22:34:30 +0300

    Tune punctuation. Support complement sets (eg. !e = any subfield $e)

--- src/reducers/punctuation.js
+++ src/reducers/punctuation.js
@@ -1,17 +1,20 @@
+/*
+* punctuation.js -- try and fix a marc field punctuation
+*
+* Author(s): Nicholas Volk <nicholas.volk@helsinki.fi>
+*/
 import createDebugLogger from 'debug';
-import { fieldsAreIdentical } from './utils';
 
 const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 
 const stripCrap = / *[-;:,+]+$/u;
 const defaultNeedsPunc = /([a-z0-9A-Z])$/u;
+const field300NeedsPunc = /([\]a-zA-Z0-9\)]|ä)$/u
 
 const cleanX00aComma = { 'code' : 'a', 'followedBy' : '#01', 'context': /[a-z],$/u, 'remove': /,$/u };
 const cleanX00aDot = { 'code' : 'ade', 'followedBy' : 'bcdeg', 'context': /[a-z0-9]\.$/u, 'remove': /\.$/u };
 
-
-const cleanX00eDot = { 'code' : 'e', 'followedBy' : 'eg', 'context': /(esittäjä|kirjoittaja|sanoittaja|säveltäjä|toimittaja)\.$/u, 'remove': /\.$/u };
-
+const cleanX00eDot = { 'code' : 'e', 'followedBy' : 'eg', 'context': /(aja|äjä)\.$/u, 'remove': /\.$/u };
 
 const addX00aComma = { 'add': ',', 'code' : 'abcde', 'followedBy' : 'deg', 'context': defaultNeedsPunc };
 const addX00aDot = { 'add': '.', 'code' : 'abcde', 'followedBy' : '#t01', 'context': defaultNeedsPunc };
@@ -20,86 +23,22 @@ const addX00aDot = { 'add': '.', 'code' : 'abcde', 'followedBy' : '#t01', 'conte
 const cleanPunctuationRules = {
   '100' : [ cleanX00aComma, cleanX00aDot, cleanX00eDot ],
   '300' : [ { 'code' : 'a', 'followedBy' : '!b', 'remove' : ' :' },
-            { 'code' : 'ab', 'followedBy' : '!c', 'remove' : ' ;' }
+            { 'code' : 'ab', 'followedBy' : '!c', 'remove' : ' ;' },
+            { 'code' : 'abc', 'followedBy' : '!e', 'remove' : ' +' }
   ],
   '700' : [ cleanX00aComma, cleanX00aDot, cleanX00eDot]
 }
 
 const addPairedPunctuationRules = {
   '100' : [ addX00aComma, addX00aDot ],
+  '300' : [ { 'code': 'a', 'followedBy': 'b', 'add': ' :', 'context': field300NeedsPunc },
+    { 'code': 'ab', 'followedBy': 'c', 'add': ' ;', 'context': field300NeedsPunc},
+    { 'code': 'abc', 'followedBy': 'e', 'add': ' +', 'context': field300NeedsPunc}
+  ],
   '700' : [ addX00aComma, addX00aDot ]
 }
 
-/*
-const pairedPunctuationRules = {
-  'b' : [ {'tag': '300', 'target': ' :', 'relevantSubfields': 'a',    'needsCleanup': stripCrap, 'cleanup_regexp': stripCrap, 'needsPunc' : defaultNeedsPunc} ],
-  'c' : [ {'tag': '300', 'target': ' ;', 'relevantSubfields': 'ab',   'needsCleanup': stripCrap, 'cleanup_regexp': stripCrap, 'needsPunc' : defaultNeedsPunc} ],
-  'e' : [ {'tag': '300', 'target': ' +', 'relevantSubfields': 'abc',  'needsCleanup': stripCrap, 'cleanup_regexp': stripCrap, 'needsPunc' : defaultNeedsPunc},
-          {'tag': '700', 'target': ',', 'relevantSubfields': 'abcdef', 'needsCleanup': /([a-z]å|ä|ö)\.$/u, 'cleanup_regexp': /\.$/u, 'needsPunc' : defaultNeedsPunc}. 
-]
-]
-};
 
-const finalPunctuationRules = {
-  '100' : [ { 'self': 'abceg', 'followedBy': 't0', 'cleanup_regexp': stripCrap, 'target': '.', 'needsPunc' : defaultNeedsPunc} ],
-  '100' : [ { 'self': 'd', 'followedBy': 't0', 'cleanup_regexp': stripCrap, 'target': '.', 'needsPunc' : /[^-]$/} ]
-};
-
-
-function fixFinalPunctuation(tag, subfield1, subfield2) {
-  if ( !((tag+'') in finalPunctuationRules)) {
-    debug(`No final punctuation rules found for ${tag}$${subfield1.code}`);
-    return;
-  }
-  const activeRules = finalPunctuationRules[tag].filter(rule => rule.self.includes(subfield1.code));
-  if ( activeRules.length === 0 ) {
-    debug(`No final punctuation rules found for ${tag}$${subfield1.code}`);
-    return;
-  }
-  activeRules.forEach(rule => { 
-    if ( !subfield1.value.endsWith(rule.target) && ( !subfield2 || rule.followedBy.includes(subfield2.code))) {
-      debug(`IMPLEMENT END PUNC RULE ${tag}$${subfield1.code}: target='${rule.target}' for '$${subfield1.code} ${subfield1.value}'`);
-      if ( 'needsCleanup' in rule && subfield1.value.match(rule.needsCleanup) ) {
-        subfield1.value = subfield1.value.replace(rule.cleanup_regexp, '');
-        debug(` STRIP TO '$${subfield1.code} ${subfield1.value}'`);
-      }
-      if ( 'needsPunc' in rule && subfield1.value.match(rule.needsPunc) ) {
-        subfield1.value += rule.target;     
-      }
-      debug(` FINAL '$${subfield1.code} ${subfield1.value}'`);    
-    }
-  });
-}
-
-function fixPairedPunctuation(tag, subfield1, subfield2) {
-  if ( !(subfield2.code in pairedPunctuationRules)) {
-    //debug(`No mid punctuation rules found for ${tag}$${subfield2.code}`);
-    fixFinalPunctuation(tag, subfield1, subfield2);
-    return;
-  }
-  const activeRules = pairedPunctuationRules[subfield2.code].filter(rule => rule['tag'] === tag);
-  if ( activeRules.length === 0 ) {
-    //debug(`No punctuation rules found for ${tag}$${subfield2.code}`);
-    fixFinalPunctuation(tag, subfield1, subfield2);
-    return;
-  }
-
-  activeRules.forEach(rule => { 
-    if ( !subfield2.value.endsWith(rule.target) && rule.relevantSubfields.includes(subfield1.code)) {
-      debug(`IMPLEMENT RULE ${tag}$${subfield2.code}: target='${rule.target}' for '$${subfield1.code} ${subfield1.value} $${subfield2.code} ${subfield2.value}'`);
-      if ( 'needsCleanup' in rule && subfield1.value.match(rule.needsCleanup) ) {
-        subfield1.value = subfield1.value.replace(rule.cleanup_regexp, '');
-        debug(` STRIP TO '$${subfield1.code} ${subfield1.value} $${subfield2.code} ${subfield2.value}'`);
-      }
-      if ( 'needsPunc' in rule && subfield1.value.match(rule.needsPunc) ) {
-        subfield1.value += rule.target;     
-      }
-      debug(` FINAL '$${subfield1.code} ${subfield1.value} $${subfield2.code} ${subfield2.value}'`);    
-    }
-  });
-}
-
-*/
 function checkRule(rule, subfield1, subfield2) {
   if ( !rule.code.includes(subfield1.code) ) {
     return false;
@@ -108,13 +47,21 @@ function checkRule(rule, subfield1, subfield2) {
     return false;
   }
 
-  if ( subfield2 === null && !rule.followedBy.includes('#') ) {
+  if ( subfield2 === null && !rule.followedBy.includes('!') && !rule.followedBy.includes('#') ) {
     return false;
   }
-  if ( subfield2 !== null && !rule.followedBy.includes(subfield2.code)) {
-    return false;
+  if ( subfield2 !== null ) {
+    if ( !rule.followedBy.includes('!') ) {
+      if ( !rule.followedBy.includes(subfield2.code)) {
+        return false;
+      }
+    }
+    else if ( rule.followedBy.includes(subfield2.code) ) { // double negation
+      return false;
+    }
   }
-  debug(` ACCEPT ${rule.code}/${subfield1.code}, SF2=${rule.followedBy}/${subfield2 ? subfield2.code : 'N/A'}}`);
+
+  debug(` ACCEPT ${rule.code}/${subfield1.code}, SF2=${rule.followedBy}/${subfield2 ? subfield2.code : 'N/A'}`);
   return true;
 }
 
@@ -126,8 +73,11 @@ function removeCrappyPunctuation(tag, subfield1, subfield2) {
   const activeRules = cleanPunctuationRules[tag].filter(rule => checkRule(rule, subfield1, subfield2));
 
   activeRules.forEach(rule => {
+    const originalValue = subfield1.value;
     subfield1.value = subfield1.value.replace(rule.remove, '');
-    debug(` POST-CRAPPY PUNC: '$${subfield1.code} ${subfield1.value}'`);
+    if ( subfield1.value !== originalValue )  {
+      debug(` REMOVE PUNC: '$${subfield1.code} ${originalValue}' => '$${subfield1.code} ${subfield1.value}'`);
+    }
   });
 }
 
@@ -149,16 +99,6 @@ function subfieldFixPunctuation(tag, subfield1, subfield2) {
   removeCrappyPunctuation(tag, subfield1, subfield2);
 
   addPairedPunctuation(tag, subfield1, subfield2);
-
-  /*
-  if ( subfield2 === null ) {
-    //debug(`No mid-punctuation rules found for $${subfield2.code}`);
-    return fixFinalPunctuation(tag, subfield1, subfield2);
-  }
-  debug(`sFP Proceed with ${subfield1.code} ${subfield1.value}`);
-  debug(`Proceed with ${subfield2.code} ${subfield2.value}`);
-  fixPairedPunctuation(tag, subfield1, subfield2);
-  */
 } 
 
 export function fieldFixPunctuation(field) {
