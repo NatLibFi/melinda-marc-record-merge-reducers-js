a40dd14996a0a6f7f36f2870520bc4a9f2b6f374
nvolk <nicholas.volk@helsinki.fi>
2021-09-30 22:32:19 +0300

    Lint. Use external field end punctuation.

--- src/reducers/punctuation.js
+++ src/reducers/punctuation.js
@@ -5,9 +5,9 @@
 *
 * TODO: implement https://www.kiwi.fi/display/kumea/Loppupisteohje
 */
-import { validateField } from '@natlibfi/marc-record-validators-melinda/dist/ending-punctuation';
+import {validateSingleField} from '@natlibfi/marc-record-validators-melinda/dist/ending-punctuation';
 import createDebugLogger from 'debug';
-import { fieldToString } from './utils';
+import {fieldToString} from './utils';
 
 const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 
@@ -27,10 +27,10 @@ const cleanX00aDot = {'code': 'abcde', 'followedBy': 'cdegj', 'context': /(?:[a-
 // These $e dot removals are tricky: before removing the comma, we should know that it ain't an abbreviation...
 const cleanX00eDot = {'code': 'e', 'followedBy': 'egj', 'context': /(?:aja|jä)\.$/u, 'remove': /\.$/u};
 
-const X00RemoveDotAfterBracket = {'code': 'cq', 'context': /\)\.$/, 'remove': /\.$/u };
+const X00RemoveDotAfterBracket = {'code': 'cq', 'context': /\)\.$/, 'remove': /\.$/u};
 
 
-const addX00aComma = {'add': ',', 'code': 'abcdej', 'followedBy': 'cdeg', 'context': commaNeedsPuncAfter, 'contextRHS': allowsPuncRHS } ;
+const addX00aComma = {'add': ',', 'code': 'abcdej', 'followedBy': 'cdeg', 'context': commaNeedsPuncAfter, 'contextRHS': allowsPuncRHS};
 const addX00aDot = {'add': '.', 'code': 'abcde', 'followedBy': '#t01', 'context': defaultNeedsPuncAfter};
 
 
@@ -44,17 +44,15 @@ const cleanPunctuationRules = {
   '600': [removeX00Comma, cleanX00aDot, cleanX00eDot, cleanX00dCommaOrDot, X00RemoveDotAfterBracket],
   '700': [removeX00Comma, cleanX00aDot, cleanX00eDot, cleanX00dCommaOrDot, X00RemoveDotAfterBracket, cleanRHS],
   '800': [removeX00Comma, cleanX00aDot, cleanX00eDot, cleanX00dCommaOrDot, X00RemoveDotAfterBracket],
-  '110': [removeX00Comma, cleanX00aDot, cleanX00eDot ],
-  '245': [
-    {'code': 'ab', 'followedBy': '!c', 'remove': ' /'}
-  ]
+  '110': [removeX00Comma, cleanX00aDot, cleanX00eDot],
+  '245': [{'code': 'ab', 'followedBy': '!c', 'remove': ' /'}]
 };
 
 const addPairedPunctuationRules = {
   '100': [addX00aComma, addX00aDot],
   '245': [
-    {'code': 'a', 'followedBy': 'b', 'add': ' :', 'context': defaultNeedsPuncAfter },
-    {'code': 'abnp', 'followedBy': 'c', 'add': ' /', 'context': defaultNeedsPuncAfter }
+    {'code': 'a', 'followedBy': 'b', 'add': ' :', 'context': defaultNeedsPuncAfter},
+    {'code': 'abnp', 'followedBy': 'c', 'add': ' /', 'context': defaultNeedsPuncAfter}
   ],
   '300': [
     {'code': 'a', 'followedBy': 'b', 'add': ' :', 'context': field300NeedsPunc},
@@ -76,7 +74,7 @@ function ruleAppliesToSubfield(rule, subfield) {
 }
 
 function ruleAppliesToNextSubfieldCode(rule, subfield) {
-  if ( !('followedBy' in rule) ) { // No sanity check required
+  if (!('followedBy' in rule)) { // No sanity check required
     return true;
   }
   const negation = rule.followedBy.includes('!');
@@ -94,7 +92,7 @@ function ruleAppliesToNextSubfieldCode(rule, subfield) {
 }
 
 function ruleAppliesToNextSubfield(rule, subfield) {
-  if ( !ruleAppliesToNextSubfieldCode(rule, subfield)) {
+  if (!ruleAppliesToNextSubfieldCode(rule, subfield)) {
     return false;
   }
   if ('contextRHS' in rule && !subfield.value.match(rule.contextRHS)) {
@@ -156,15 +154,15 @@ function subfieldFixPunctuation(tag, subfield1, subfield2) {
 function getFinalPunctuationSubfield264(field, subfield) {
   // "Copyright-vuoden kanssa ei käytetä loppupistettä (2. indikaattori = 4)."
   // "Must be $a condition" is aped from marc-record-validators-melinda/src/ending-punctuation-conf.js.
-  if ( field.ind2 === 4 || subfield.code !== 'c' ) {
+  if (field.ind2 === 4 || subfield.code !== 'c') {
     return false;
   }
-  
+
   // "264-kenttään tulee loppupiste, JOS on käytetty osakenttää ‡c tuotantoajan, julkaisuajan, jakeluajan tai valmistusajan ilmaisemiseen
   // (2. indikaattori = 0, 1, 2 tai 3) JA osakenttä ‡c ei pääty hakasulkuun ']' tai tavuviivaan '-'   tai kaarisulkuun ')'  tai kysymysmerkkiin '?'
   // NB! No need to check ind2 as the only other possible value has already been covered.
   // NB! Can be use the generic punc regexp here?
-  if ( subfield.value.match(/[\-\])?.]$/u)) {
+  if (subfield.value.match(/[\-\])?.]$/u)) {
     return false;
   }
   return subfield;
@@ -172,22 +170,22 @@ function getFinalPunctuationSubfield264(field, subfield) {
 
 function getRelevantSubfields(field) {
   // Skip non-interesting fields:
-  if ( !field.tag.match(/^(?:036|051|[1678](?:00|10|11|30)|242|245|250|260|264|307|340|343|351|352|362|50[0-9]|51[1-8]|52[0-6]|53[0348]|54[014567]|55[0256]|56[1237]|58[01458]|720|740|752|754|76[0-9]|77[0-9]|78[0-7]|880)$/u) ) {
+  if (!field.tag.match(/^(?:036|051|[1678](?:00|10|11|30)|242|245|250|260|264|307|340|343|351|352|362|50[0-9]|51[1-8]|52[0-6]|53[0348]|54[014567]|55[0256]|56[1237]|58[01458]|720|740|752|754|76[0-9]|77[0-9]|78[0-7]|880)$/u)) {
     return [];
   }
   // Pick subfields:
   return field.subfields.filter(subfield => {
-    if ( "uw0123456789".includes(subfield.code)) {
+    if ('uw0123456789'.includes(subfield.code)) {
       return false;
     }
-    if ( field.tag === '242' && subfield.code === 'y' ) {
+    if (field.tag === '242' && subfield.code === 'y') {
       return false;
     }
 
-    if ( field.tag === '506' && subfield.code === 'f' ) {
+    if (field.tag === '506' && subfield.code === 'f') {
       return false;
     }
-    if ( subfield.code === 'u' && field.tag in [ '520', '538', ]) {
+    if (subfield.code === 'u' && field.tag in ['520', '538']) {
       return false;
     }
     return true;
@@ -197,43 +195,44 @@ function getRelevantSubfields(field) {
 function getFinalPunctuationSubfield(field) {
   const relevantSubfields = getRelevantSubfields(field);
   const index = relevantSubfields.length - 1;
-  if ( index < 0 ) {
+  if (index < 0) {
     return null;
   }
-  // Already has punctuation ("Välimerkit: .?-!") :  
-  if ( '.?-!'.includes(relevantSubfields[index].value.slice(-1)) ) {
+  // Already has punctuation ("Välimerkit: .?-!") :
+  if ('.?-!'.includes(relevantSubfields[index].value.slice(-1))) {
     return null;
   }
 
   // Exceptions:
   // X00, X10, X11, X30 and 740:
-  if ( field.tag.match(/^(?:[1678]00|[1678]10|[1678]11|[1678]30|740)$/u) ) {
-    if ( relevantSubfields[index].value.slice(-1) === ')' ) {
+  if (field.tag.match(/^(?:[1678]00|[1678]10|[1678]11|[1678]30|740)$/u)) {
+    if (relevantSubfields[index].value.slice(-1) === ')') {
       return null; // Is this really an expection. See 260 specs...
     }
   }
 
-  if ( field.tag === '264' ) {
+  if (field.tag === '264') { // Exceptionally indicators affect punctuation
     return getFinalPunctuationSubfield264(field, relevantSubfields[index]);
   }
-  
 
-  if ( field.tag === '340' && 'cdgjkmop'.includes(relevantSubfields[index].code) ) {
+
+  if (field.tag === '340' && 'cdgjkmop'.includes(relevantSubfields[index].code)) {
     return null;
   }
-  if ( field.tag.match(/^(?:647|648|65[0145678]|662)$/u) ) {
+  
+  if (field.tag.match(/^(?:647|648|65[0145678]|662)$/u)) {
     // "EI suomalaisten sanastojen termeihin, muihin sanaston käytännön mukaan, yleensä KYLLÄ"
     // NB! As we are Finns, we default to our way.
     // We should add punc to most of the non-Finnish lexicons.. Will list them as exceptions here eventually.
     return null;
   }
 
-  if ( field.tag === '036' && relevantSubfields[index].code !== 'b' ) {
+  if (field.tag === '036' && relevantSubfields[index].code !== 'b') {
     return false;
   }
 
   // Fields 567 and 760...788:
-  if ( relevantSubfields[index].code !== 'a' && field.tag.match(/^(?:567|76.|77.|78.)$/u) ) {
+  if (relevantSubfields[index].code !== 'a' && field.tag.match(/^(?:567|76.|77.|78.)$/u)) {
     // Funny, we don't want $a in 773 anyway...
     return null;
   }
@@ -243,7 +242,7 @@ function getFinalPunctuationSubfield(field) {
 function addFinalPunctuation(field) {
   // Add punctuation as per https://www.kiwi.fi/display/kumea/Loppupisteohje:
   const subfield = getFinalPunctuationSubfield(field);
-  if ( subfield && subfield.value.slice(-1) !== '.' )  {
+  if (subfield && subfield.value.slice(-1) !== '.') {
     debug(`  Adding final punctuation '.' to '${field.tag} $${subfield.code} ${subfield.value}'.`);
     subfield.value += '.';
   }
@@ -259,7 +258,7 @@ export function fieldFixPunctuation(field) {
   field.subfields.forEach((sf, i) => {
     subfieldFixPunctuation(field.tag, sf, i + 1 < field.subfields.length ? field.subfields[i + 1] : null);
   });
-  addFinalPunctuation(field); // Attempt to use the (modified version of) existing stuff. Nothings happens as of now.
-  //validateField(field, field.tag, true);
+  //addFinalPunctuation(field); // Attempt to use the (modified version of) existing stuff. Nothings happens as of now.
+  validateSingleField(field, false, true); // NB! Don't use field.tag as second argument! It's a string, not an int.
   return field;
 }
