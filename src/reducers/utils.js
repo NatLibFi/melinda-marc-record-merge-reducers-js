1ae96729db45ed5d5b8500d97acab75c7dfabc54
nvolk <nicholas.volk@helsinki.fi>
2021-11-06 22:31:42 +0200

    New function: isControlSubfieldCode(code)

--- src/reducers/utils.js
+++ src/reducers/utils.js
@@ -229,3 +229,7 @@ export function recordReplaceField(record, originalField, newField) {
   return record;
 }
 
+export function isControlSubfieldCode(subfieldCode) {
+  if ( ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'w'].includes(subfieldCode) ) { return true; }
+  return false;
+}
