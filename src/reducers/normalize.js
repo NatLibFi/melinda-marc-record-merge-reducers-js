a04dda50182391f3c72851873fc05e0f7f862381
nvolk <nicholas.volk@helsinki.fi>
2021-11-09 00:01:01 +0200

    Support composition changes.

--- src/reducers/normalize.js
+++ src/reducers/normalize.js
@@ -2,9 +2,26 @@ import {normalizeSync} from 'normalize-diacritics';
 import createDebugLogger from 'debug';
 import clone from 'clone';
 import { fieldStripPunctuation } from './punctuation.js';
-import { fieldToString, isControlSubfieldCode } from './utils.js';
+import { fieldToString, isControlSubfieldCode, subfieldsAreIdentical } from './utils.js';
 
 const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
+
+
+function fixComposition(value) {
+    // Target: Diacritics use Melinda internal notation.
+    // Solution: Decompose everything and then compose 'å', 'ä', 'ö', 'Å', 'Ä' and 'Ö'.
+    return String(value).normalize('NFD').
+        replace(/å/gu, 'å').
+        replace(/ä/gu, 'ä').
+        replace(/ö/gu, 'ö').
+        replace(/Å/gu, 'Å').
+        replace(/Ä/gu, 'Ä').
+        replace(/Ö/gu, 'Ö');
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+}
+
+
 
 function dontLowercase(tag, subfieldCode) {
     if ( isControlSubfieldCode(subfieldCode) ) { return true; }
@@ -29,7 +46,7 @@ function fieldPreprocess(field) {
         // 1. Fix composition
          // I don't want to use normalizeSync(). "åäö" => "aao". Utter crap! NB: Use something else later on!
         //sf.value = normalizeSync(sf.value);
-
+        fieldFixComposition(field);
         // 2. Fix other shit
         // - non-breaking space etc whitespace characters
         // - various '-' letters?
@@ -74,6 +91,21 @@ export function cloneAndNormalizeField(field) {
     return clonedField;
 }
 
+export function fieldFixComposition(field) {
+    if ( !field.subfields ) { return field; }
+    const originalValue = fieldToString(field);
+    field.subfields.forEach((subfield, index) => field.subfields[index].value = fixComposition(subfield.value));
+    const newValue = fieldToString(field);
+    if ( originalValue !== newValue ) {
+        debug(`FIXCOMP: '${originalValue}' => '${newValue}'`);
+    }
+    return field;
+}
 
+export function recordFixComposition(record) {
+    if ( !record.fields ) { return record; }
+    record.fields.forEach((field, index) => record.fields[index] = fieldFixComposition(field));
+    return record;
+}
 
  
