f814c49277efc8a86fc8d189a482efc3a81b7ce6
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-01-21 16:04:08 +0200

    remake tests

--- src/reducers/field007.spec.js
+++ src/reducers/field007.spec.js
@@ -1,24 +1,25 @@
-import chai from 'chai';
-import fs from 'fs';
-import path from 'path';
+import {expect} from 'chai';
 import {MarcRecord} from '@natlibfi/marc-record';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
 import createReducer from './field007';
-import fixturesFactory, {READERS} from '@natlibfi/fixura';
+//import createDebugLogger from 'debug';
 
-MarcRecord.setValidationOptions({subfieldValues: false});
-
-describe('reducers/field007', () => {
-  const {expect} = chai;
-  const fixturesPath = path.join(__dirname, '..', '..', 'test-fixtures', 'reducers', 'field007');
-
-  fs.readdirSync(fixturesPath).forEach(subDir => {
-    const {getFixture} = fixturesFactory({root: [fixturesPath, subDir], reader: READERS.JSON, failWhenNotFound: false});
-    it(subDir, () => {
-      const base = new MarcRecord(getFixture('base.json'));
-      const source = new MarcRecord(getFixture('source.json'));
-      const expectedRecord = getFixture('merged.json');
-      const mergedRecord = createReducer()(base, source);
-      expect(mergedRecord.toObject()).to.eql(expectedRecord);
-    });
-  });
+generateTests({
+  callback,
+  path: [__dirname, '..', '..', 'test-fixtures', 'reducers', 'field007'],
+  recurse: false,
+  useMetadataFile: true,
+  fixura: {
+    failWhenNotFound: false,
+    reader: READERS.JSON
+  }
 });
+
+function callback({getFixture}) {
+  const base = new MarcRecord(getFixture('base.json'), {subfieldValues: false});
+  const source = new MarcRecord(getFixture('source.json'), {subfieldValues: false});
+  const expectedRecord = getFixture('merged.json');
+  const mergedRecord = createReducer()(base, source);
+  expect(mergedRecord.toObject()).to.eql(expectedRecord);
+}
--- src/reducers/leader.spec.js
+++ src/reducers/leader.spec.js
@@ -1,30 +1,32 @@
-import chai from 'chai';
-import fs from 'fs';
-import path from 'path';
+import {expect} from 'chai';
 import {MarcRecord} from '@natlibfi/marc-record';
 import createReducer from './leader';
-import fixturesFactory, {READERS} from '@natlibfi/fixura';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
 
-MarcRecord.setValidationOptions({subfieldValues: false});
-
-describe('reducers/leader', () => {
-  const {expect} = chai;
-  const fixturesPath = path.join(__dirname, '..', '..', 'test-fixtures', 'reducers', 'leader');
-
-  fs.readdirSync(fixturesPath).forEach(subDir => {
-    const {getFixture} = fixturesFactory({root: [fixturesPath, subDir], reader: READERS.JSON, failWhenNotFound: false});
-    it(subDir, () => {
-      const base = new MarcRecord(getFixture('base.json'));
-      const source = new MarcRecord(getFixture('source.json'));
-      const expectedRecord = getFixture('merged.json');
-      const expectedError = getFixture({components: ['expected-error.txt'], reader: READERS.TEXT});
-      // Bypass expected error in testing
-      if (expectedError) {
-        expect(() => createReducer.to.throw(Error, 'LDR'));
-        return;
-      }
-      const mergedRecord = createReducer()(base, source);
-      expect(mergedRecord.toObject()).to.eql(expectedRecord);
-    });
-  });
+generateTests({
+  callback,
+  path: [__dirname, '..', '..', 'test-fixtures', 'reducers', 'leader'],
+  recurse: false,
+  useMetadataFile: true,
+  fixura: {
+    failWhenNotFound: false,
+    reader: READERS.JSON
+  }
 });
+
+function callback({getFixture, expectedError = false}) {
+  const base = new MarcRecord(getFixture('base.json'), {subfieldValues: false});
+  const source = new MarcRecord(getFixture('source.json'), {subfieldValues: false});
+  const expectedRecord = getFixture('merged.json');
+  try {
+    const mergedRecord = createReducer()(base, source);
+    expect(mergedRecord.toObject()).to.eql(expectedRecord);
+  } catch (error) {
+    if (expectedError) {
+      expect(() => createReducer.to.throw(Error, 'LDR'));
+      expect(error.message).to.eql(expectedError);
+      return;
+    }
+  }
+}
