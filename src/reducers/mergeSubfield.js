81844825e87220ae042f3e31f590a253012f10ba
nvolk <nicholas.volk@helsinki.fi>
2022-01-28 13:40:04 +0200

    Switch to utils.js version on nvdebug. Log more.

--- src/reducers/mergeSubfield.js
+++ src/reducers/mergeSubfield.js
@@ -2,11 +2,11 @@ import createDebugLogger from 'debug';
 import {cloneAndRemovePunctuation, normalizeSubfield0Value} from './normalize.js';
 import {
   fieldHasSubfield,
-  fieldIsRepeatable,
+  fieldIsRepeatable, fieldToString, nvdebug,
   subfieldsAreIdentical
 } from './utils.js';
 
-const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
+const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers:mergeSubfield');
 
 const excludeSubfieldsFromMerge = [
   {'tag': '020', 'subfields': 'c'},
@@ -98,7 +98,8 @@ function replaceEntrysBirthAndDeathYear(targetField, candSubfield, relevantSubfi
 }
 
 function replaceDatesAssociatedWithName(targetField, candSubfield, relevantSubfields) {
-  // Handle X100$d: add death year, if original value only contains birth year:
+  // Add also the death year, if the original value only contains birth year.
+  // This function treats only with X00$d subfields:
   if (candSubfield.code !== 'd' || !(/^[1678]00$/u).test(targetField.tag)) {
     return false;
   }
@@ -119,7 +120,7 @@ function replaceSubfieldWithBetterValue(targetField, candSubfield) {
   // However, replacing/succeeding requires a sanity check, that the new value is a better one...
   // Thus, typically this function fails...
   const relevantSubfields = targetField.subfields.filter(subfield => subfield.code === candSubfield.code);
-  if (relevantSubfields.length === 0) { // Can't replace anything, can I...
+  if (relevantSubfields.length === 0) { // There must be something that gets replaced.
     return false;
   }
   debug(`Got ${relevantSubfields.length} sf-cands for field ${targetField.tag}‡${candSubfield.code}`);
@@ -139,7 +140,8 @@ function okToInsertTagCode(tag, code) {
 
 function insertSubfieldAllowed(targetField, candSubfield) {
   // NB! If insert is not allowed, the candicate subfield can still replace the original. (Not handled by this function though.)
-  // Subfield codes missing from the original record can be added by defautl:
+
+  // Subfield codes missing from the original record can be added by default:
   if (!fieldHasSubfield(targetField, candSubfield.code) && okToInsertTagCode(targetField.tag, candSubfield.code)) {
     return true;
   }
@@ -198,13 +200,13 @@ function mergeSubfieldNotRequiredSpecialCases(targetField, candSubfield) {
   // Add hard-coded exceptions here
   if (targetField.tag === '040' && candSubfield.code === 'd' &&
       targetField.subfields.some(sf => sf.code === 'a' && sf.value === candSubfield.value)) {
-    debug('040$d matched 040$a');
+    debug('040‡d matched 040‡a');
     return true;
   }
   if (candSubfield.code === 'g' && candSubfield.value === 'ENNAKKOTIETO.') {
-    // Skip just $g subfield or the whole field?
+    // Skip just ‡g subfield or the whole field?
     // We decided to skip just this subfield. We want at least $0 and maybe more even from ennakkotieto.
-    debug('Skip $g ENNAKKOTIETO.');
+    debug('Skip ‡g ENNAKKOTIETO.');
     return true;
   }
   // Don't add $0 subfields that mean the same even if they look different:
@@ -219,9 +221,11 @@ function mergeSubfieldNotRequired(targetField, candSubfield) {
   // candSubfield has been stripped of punctuation
   const normalizedTargetField = cloneAndRemovePunctuation(targetField);
 
+  nvdebug(`     Look for identical subfields in '${fieldToString(normalizedTargetField)}'`);
+
   if (normalizedTargetField.subfields.some(sf => subfieldsAreIdentical(sf, candSubfield))) {
     // Subfield with identical normalized value exists. Do nothing.
-    // Not ideal 382$n subfields, I guess...
+    // Not ideal 382‡n subfields, I guess... Nor 505‡trg repetitions... These need to be fixed...
     return true;
   }
   if (mergeSubfieldNotRequiredSpecialCases(targetField, candSubfield)) {
@@ -285,7 +289,7 @@ export function bottomUpSortSubfields(field) {
 
 function addSubfield(targetField, candSubfield) {
   const str = `${candSubfield.code} ${candSubfield.value}`;
-  debug(` Added subfield ‡'${str}' to field`);
+  nvdebug(` Added subfield ‡'${str}' to field`, debug);
   // Add subfield to the end of all subfields. NB! Implement a separate function that does this + subfield reordering somehow...
   targetField.subfields.push(candSubfield); // eslint-disable-line functional/immutable-data
   targetField.merged = 1; // eslint-disable-line functional/immutable-data
@@ -293,19 +297,27 @@ function addSubfield(targetField, candSubfield) {
 }
 
 export function mergeSubfield(record, targetField, candSubfield) {
+  nvdebug(`   Q: mergeSubfield '‡${candSubfield.code} ${candSubfield.value}' to field '${fieldToString(targetField)}'?`, debug);
   if (mergeSubfieldNotRequired(targetField, candSubfield)) {
-    //debug(`    No need to add subfield '‡${candSubfield.code} ${candSubfield.value}'`);
+    nvdebug(`    A: No. No need to merge subfield '‡${candSubfield.code} ${candSubfield.value}'`, debug);
     return;
   }
+
   if (insertSubfieldAllowed(targetField, candSubfield)) {
+    nvdebug(`    A: Yes. Add subfield '‡${candSubfield.code} ${candSubfield.value}'`, debug);
+
     addSubfield(targetField, candSubfield);
     return;
   }
-  // Currently only X00$d 1984- => 1984-2000 type of changes
+
+  // Currently only X00$d 1984- => 1984-2000 type of changes.
+  // It all other cases the original subfield is kept.
   if (replaceSubfieldWithBetterValue(targetField, candSubfield)) {
+    nvdebug(`    A: Yes. Subfield '‡${candSubfield.code} ${candSubfield.value}' replaces and original subfield.`, debug);
     targetField.merged = 1; // eslint-disable-line functional/immutable-data
     return;
   }
+
   // Didn't do anything, but thinks something should have been done:
-  debug(`TODO: Handle merging/adding subfield '‡${candSubfield.code} ${candSubfield.value}' to field`);
+  nvdebug(`    A: Could not decide. Add decision rules. 'Til then, do nothing to '‡${candSubfield.code} ${candSubfield.value}'`, debug);
 }
