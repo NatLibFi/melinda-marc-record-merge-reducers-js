804dd9b6e760033f3f55005e642c39a0f6225923
nvolk <nicholas.volk@helsinki.fi>
2022-02-15 10:47:44 +0200

    Tune subfield sorting

--- src/reducers/mergeSubfield.js
+++ src/reducers/mergeSubfield.js
@@ -21,22 +21,22 @@ const includeSubfields = [{'tag': '040', 'subfields': 'abcde68'}]; // if we want
 const subfieldSortOrder = [
   {'tag': '040', 'sortOrder': ['8', '6', 'a', 'b', 'c', 'e', 'd', 'x']},
   {'tag': '048', 'sortOrder': ['8', '6', 'b', 'a']},
-  {'tag': '100', 'sortOrder': ['a', 'b', 'c', 'q', 'd', 'e', 'j', 't', 'u', 'l', 'f', '0', '5', '9']}, // don't do $g
+  {'tag': '100', 'sortOrder': ['6', 'a', 'b', 'c', 'q', 'd', 'e', 'j', 't', 'u', 'l', 'f', '0', '5', '9']}, // don't do $g
   {'tag': '110', 'sortOrder': ['a', 'b', 'n']},
   {'tag': '111', 'sortOrder': ['a', 'n', 'd', 'c', 'e', 'g', 'j']},
   {'tag': '130', 'sortOrder': ['a', 'n', 'p', 'k', 'l']},
   {'tag': '240', 'sortOrder': ['a', 'm', 'n', 'p', 's', 'l', '2', '0', '1', '5', '9']},
-  {'tag': '245', 'sortOrder': ['a', 'b', 'n', 'p', 'k', 'f', 'c']},
+  {'tag': '245', 'sortOrder': ['6', 'a', 'b', 'n', 'p', 'k', 'f', 'c']},
   {'tag': '382', 'sortOrder': ['a']},
   {'tag': '385', 'sortOrder': ['8', 'm', 'n', 'a']},
   {'tag': '386', 'sortOrder': ['8', 'm', 'n', 'a']},
   {'tag': '490', 'sortOrder': ['a', 'x', 'v', 'l']},
   {'tag': '505', 'sortOrder': ['a']},
   {'tag': '526', 'sortOrder': ['i', 'a', 'b', 'x', 'z']},
-  {'tag': '600', 'sortOrder': ['a', 'b', 'c', 'q', 'd', 'e', '0', '5', '9']},
-  {'tag': '610', 'sortOrder': ['a', 'b', 'n']},
+  {'tag': '600', 'sortOrder': ['6', 'a', 'b', 'c', 'q', 'd', 'e', '0', '5', '9']},
+  {'tag': '610', 'sortOrder': ['6', 'a', 'b', 'n']},
   {'tag': '611', 'sortOrder': ['a', 'n', 'd', 'c', 'e', 'g', 'j']},
-  {'tag': '700', 'sortOrder': ['i', 'a', 'b', 'c', 'q', 'd', 'e', 't', 'u', 'l', 'f', '0', '5', '9']},
+  {'tag': '700', 'sortOrder': ['6', 'i', 'a', 'b', 'c', 'q', 'd', 'e', 't', 'u', 'l', 'f', '0', '5', '9']},
   {'tag': '710', 'sortOrder': ['a', 'b', 'n']},
   {'tag': '711', 'sortOrder': ['a', 'n', 'd', 'c', 'e', 'g', 'j']},
   {'tag': '773', 'sortOrder': ['7', 'w', 'a', 't', 'd', 'm', 'h', 'g', 'k', 'o', 'x', 'z', 'g', 'q']},
@@ -264,6 +264,7 @@ function swapSubfields(field, sortOrder) {
     return true;
   });
 
+
   if (loopAgain) {
     return swapSubfields(field, sortOrder);
   }
@@ -271,19 +272,23 @@ function swapSubfields(field, sortOrder) {
   return;
 }
 
-const defaultSortOderString = '8673abcdefghijklmnopqrstuvwxyz420159';
+const defaultSortOrderString = '8673abcdefghijklmnopqrstuvwxyz420159';
+const defaultSortOrder = defaultSortOrderString.split('');
+
 export function bottomUpSortSubfields(field) {
   // Features:
   // - Swap only sort adjacent pairs.
   // - No sorting over unlisted subfield codes. Thus a given subfield can not shift to wrong side of 700$t...
 
-  // Should we support multiple values?
+  // Implement: 880 field should use values from $6...
+
+  // Should we support multiple sort orders per field?
   const sortOrder = getSubfieldSortOrder(field);
 
   //if (sortOrder === null) { return field; } //// Currently always sort..
 
   swapSubfields(field, ['8', '6', '7', '3', 'a', '4', '2', '0', '1', '5', '9']); // <= Handle control subfield order (it never changes)
-  swapSubfields(field, sortOrder || defaultSortOderString);
+  swapSubfields(field, sortOrder || defaultSortOrder);
 
   return field;
 }
