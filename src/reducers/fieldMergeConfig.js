10c5817c97b9f447efa5adef7498ca121a5f7364
nvolk <nicholas.volk@helsinki.fi>
2022-08-10 11:21:06 +0300

    support option "indicator1PreferredValues"

--- src/reducers/fieldMergeConfig.js
+++ src/reducers/fieldMergeConfig.js
@@ -10,6 +10,7 @@ export function initFieldMergeConfig(initData = {}) {
     ignoreIndicator2: initData.ignoreIndicator2 || false,
     // When fields are merged despite of difference in indicator value, typically base field's value is kept.
     // However, source's value might be used if indicator value preference is defined (per tag):
+    indicator1PreferredValues: initData.indicator1PreferredValues && typeof initData.indicator1PreferredValues === 'object' ? initData.indicator1PreferredValues : false,
     indicator2PreferredValues: initData.indicator2PreferredValues && typeof initData.indicator2PreferredValues === 'object' ? initData.indicator2PreferredValues : false,
 
     // skipAddTags: list of tags, that prevent adding. If empty, hard-coded defaults/educated guesses are used.
