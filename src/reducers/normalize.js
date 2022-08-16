17b858f40309b8950a3916c1e91e1136e510dcf4
nvolk <nicholas.volk@helsinki.fi>
2022-08-16 14:32:18 +0300

    remove externalFixes(). Implemented in hardcodedSourcePreprocessor.js.

--- src/reducers/normalize.js
+++ src/reducers/normalize.js
@@ -2,9 +2,6 @@ import clone from 'clone';
 import {fieldStripPunctuation} from './punctuation.js';
 import {fieldToString, isControlSubfieldCode, nvdebug} from './utils.js';
 
-import fieldExclusion from '@natlibfi/marc-record-validators-melinda/dist/field-exclusion';
-import subfieldExclusion from '@natlibfi/marc-record-validators-melinda/dist/subfield-exclusion';
-import isbnIssn from '@natlibfi/marc-record-validators-melinda/dist/isbn-issn';
 import {default as normalizeEncoding, fieldFixComposition, fieldRemoveDecomposedDiacritics} from './normalizeEncoding';
 import {fieldNormalizePrefixes} from './normalizeIdentifier';
 import {getMaxSubfield6, reindexSubfield6s} from './reindexSubfield6.js';
@@ -149,51 +146,11 @@ export function cloneAndNormalizeField(field) {
 }
 
 
-function externalFixes(record) {
-  //externalFieldsPresent(record, [/^336$/u, /^337$/u], true); // Comps don't always have 338
-  //await FieldsPresent([/^336$/u, /^337$/u, /^338$/u]), // Comps don't always have 338
-
-
-  const fieldExcluder = fieldExclusion([
-    // /^(001|091|092|093|094|095|256|533|574|575|576|577|578|599)$/,
-    //{tag: /^264$/, subfields: [{code: /^a$/, value: /^\[.*\]$/}]}, // Not sure about this either
-    //{tag: /^650$/, subfields: [{code: /^a$/, value: /^overdrive$/i}]}, // Not sure what this is
-    //{tag: /^041$/u, dependencies: [{leader: /^.{6}[g|i]/u}]},
-    {tag: /^(?:648|650|651|655)$/u, subfields: [{code: /^2$/u, value: /^(?:ysa|musa|allars|cilla)$/u}]}
-  ]);
-
-  fieldExcluder.fix(record);
-
-  const subfieldExcluder = subfieldExclusion([
-    {tag: /^041$/u, subfields: [{code: /^[ad]$/u, value: /^zxx$/u}]},
-    {tag: /^02[04]$/u, subfields: [{code: /^c$/u, value: /^.*(?:€|£|\$|FIM).*$/u}]} // price info
-  ]);
-
-  subfieldExcluder.fix(record);
-
-  const addHyphensToISBN = isbnIssn({hyphenateISBN: true});
-
-  addHyphensToISBN.fix(record);
-
-  /*
-  await EmptyFields(),
-  await IsbnIssn({hyphenateISBN: true}),
-  await SubfieldExclusion([
-    {tag: /^041$/u, subfields: [{code: /^[ad]$/u, value: /^zxx$/u}]},
-    {tag: /^02[04]$/u, subfields: [{code: /^c$/u, value: /^.*(?:€|£|\$|FIM).*$/u}]} // price info
-  ]),
-  //await FieldStructure([{tag: /^007$/u, dependencies: [{leader: /^.{6}[^at]/u}]}]),
-  await Punctuation(),
-  await EndingPunctuation()
-  */
-  return record;
-}
-
 export function recordPreprocess(record) { // For both base and source record
   if (!record.fields) {
     return record;
   }
-  externalFixes(record); // Fixes from outside this module
+  // externalFixes(record); // Fixes from outside this module
 
   //record = result.record; // eslint-disable-line functional/immutable-data
   normalizeEncoding().fix(record);
