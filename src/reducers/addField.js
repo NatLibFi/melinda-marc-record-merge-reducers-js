5e4dd841b161c1865acf642c14ce9c82e420f0e8
nvolk <nicholas.volk@helsinki.fi>
2022-08-17 16:25:24 +0300

    Add default function

--- src/reducers/addField.js
+++ src/reducers/addField.js
@@ -5,6 +5,10 @@ import {cloneAndPreprocessField} from './mergePreAndPostprocess';
 import {isSubfieldGoodForMerge} from './mergeSubfield';
 import {addableTag} from './mergableTag';
 
+import {MarcRecord} from '@natlibfi/marc-record';
+import {initFieldMergeConfig} from './fieldMergeConfig.js';
+import {postprocessRecord} from './mergePreAndPostprocess.js';
+import {recordPreprocess/*, sourceRecordPreprocess*/} from './normalize.js';
 
 //import {sortAdjacentSubfields} from './sortSubfields';
 // import identicalFields from '@natlibfi/marc-record-validators-melinda/dist/identical-fields';
@@ -15,6 +19,35 @@ const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers:ad
 
 // Default list of (repeatable) fields that we don't copy if the field is already present in the base record
 const defaultDoNotCopyIfFieldPresentRegexp = /^(?:041|260|264|300|310|321|335|336|337|338)/u;
+
+export default (config = {}) => (base, source) => {
+
+  const baseRecord = new MarcRecord(base, {subfieldValues: false});
+  const sourceRecord = new MarcRecord(source, {subfieldValues: false});
+
+  //nvdebug(`OBJ: ${JSON.stringify(config)}`);
+  // How do we read the config? Config file? Parameters from calling function? Currently this just sets the defaults...
+  const processedConfig = initFieldMergeConfig(config);
+
+  // We should clone the records here and just here...
+  const baseRecord2 = recordPreprocess(baseRecord); // fix composition et al
+  const sourceRecord2 = recordPreprocess(sourceRecord); // fix composition et al
+
+  const defCandFieldsRegexp = /^(?:0[1-9][0-9]|[1-9][0-9][0-9]|CAT|LOW|SID)$/u;
+
+  const candidateFields = sourceRecord2.get(processedConfig.tagPattern ? processedConfig.tagPattern : defCandFieldsRegexp);
+  //  .filter(field => !isMainOrCorrespondingAddedEntryField(field)); // current handle main entries as well
+
+  candidateFields.forEach(candField => {
+    nvdebug(`add field: Now processing ${fieldToString(candField)}`, debug);
+    addField(baseRecord2, candField, processedConfig);
+  });
+
+  postprocessRecord(baseRecord2);
+  postprocessRecord(sourceRecord2);
+
+  return [baseRecord2, sourceRecord2];
+};
 
 function getConfigDoNotCopyIfFieldPresentAsRegexp(config) {
   if (config.doNotCopyIfFieldPresent) {
