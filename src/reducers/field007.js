8c59bd5b764518b2e673322f0ebdc0b7eeaa74e2
nvolk <nicholas.volk@helsinki.fi>
2022-08-24 16:19:12 +0300

    Rewrite functionality. Ignore specs, and use common sense.

--- src/reducers/field007.js
+++ src/reducers/field007.js
@@ -1,25 +1,45 @@
 import {MarcRecord} from '@natlibfi/marc-record';
 import createDebugLogger from 'debug';
-import {copyFields, fieldToString, getEncodingLevelRanking} from './utils.js';
+import {mergeControlFields} from './controlFieldUtils.js';
+import {copyFields /*, fieldToString, getEncodingLevelRanking*/} from './utils.js';
 
-// Test 04: If 007/00-01 are different in base and source, copy 007 from source to base as new field (2x)
-// Test 05: If 007/00-01 are the same, keep existing field 007 in base (2x)
 
 const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 
 // NB: handle merging better.ns
 export default () => (base, source) => {
 
+  /*
+  // NV: Nah, enc level is prolly irrelevant here. We have already chosen base somewhere. Let's trust that decision instead.
   if (getEncodingLevelRanking(base) < getEncodingLevelRanking(source)) { // smaller is better!
     // The original version is better than the alternative version.
     return base;
   }
+  */
   const baseRecord = new MarcRecord(base, {subfieldValues: false});
   const sourceRecord = new MarcRecord(source, {subfieldValues: false});
 
   const baseFields = baseRecord.get(/^007$/u);
   const sourceFields = sourceRecord.get(/^007$/u);
 
+  if (baseFields.length === 0 && sourceFields.length > 0) {
+    debug(`Copy ${sourceFields.length} source field(s), since host has no 007`);
+    copyFields(baseRecord, sourceFields);
+    return baseRecord;
+  }
+
+  // NV: added this
+  if (baseFields.length === 1) {
+    if (sourceFields.length === 1) {
+      if (allowMerge(baseFields[0].value, sourceFields[0].value)) {
+        mergeControlFields(baseFields[0], sourceFields[0]); // eslint-disable-line functional/immutable-data
+
+        return baseRecord;
+      }
+    }
+  }
+  // The rest is someone else's code. Complies with specs, which are bad. Not doing them :D
+  /*
   const mergableFields = sourceFields.filter(sf => baseFields.every(bf => allowCopy(bf, sf)));
 
   //const nonIdenticalFields = getNonIdenticalFields(baseFields, sourceFields);
@@ -28,18 +48,24 @@ export default () => (base, source) => {
     debug(`${mergableFields.length} copyable field(s)`);
     return copyFields(baseRecord, mergableFields);
   }
-
+  */
   return base;
 
-  function allowCopy(baseField, sourceField) {
-    // Copy source field if source 007/00 and/or 007/01 are different from base
-    if (baseField.value[0] === sourceField.value[0]) {
-      if (baseField.value[1] === sourceField.value[1] || sourceField.value[1] === '|') {
-        // If 007/00 and 01 are identical, keep base field
-        debug(`Don't copy ${fieldToString(sourceField)}`);
-        return false;
-      }
+  function allowMerge(baseValue, sourceValue) {
+    // Copy
+    if (baseValue.length < 2 || sourceValue.length < 2) {
+      return false;
     }
-    return true;
+
+    if (baseValue.charAt(0) !== sourceValue.charAt(0)) {
+      return false;
+    }
+
+    if (baseValue.charAt(1) === sourceValue.charAt(1) ||
+        sourceValue.charAt(1) === '|' ||
+        baseValue.charAt(1) === '|') {
+      return true;
+    }
+    return false;
   }
 };
