30ac64c4721720dec2e066ee78a62a07bc9f8a96
nvolk <nicholas.volk@helsinki.fi>
2022-08-30 14:17:17 +0300

    Add test specs for field 240

---
+++ src/reducers/field240.spec.js
@@ -0,0 +1,46 @@
+import {expect} from 'chai';
+import {MarcRecord} from '@natlibfi/marc-record';
+import createReducer from './field240';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+
+
+describe('merge data field tests: ', () => {
+  generateTests({
+    callback,
+    path: [__dirname, '..', '..', 'test-fixtures', 'reducers', 'field240'],
+    recurse: false,
+    useMetadataFile: true,
+    fixura: {
+      failWhenNotFound: false,
+      reader: READERS.JSON
+    }
+  });
+
+  function callback({getFixture,
+    tagPattern = false,
+    config = undefined}) {
+    const base = new MarcRecord(getFixture('base.json'), {subfieldValues: false});
+    const source = new MarcRecord(getFixture('source.json'), {subfieldValues: false});
+    const expectedRecord = getFixture('merged.json');
+    const expectedModifiedSourceRecord = getFixture('modifiedSource.json');
+
+    const marcReducers = generateReducers(tagPattern, config);
+    const [mergedRecord, modifiedSourceRecord] = marcReducers(base, source);
+    expect(mergedRecord.toObject()).to.eql(expectedRecord);
+    expect(modifiedSourceRecord.toObject()).to.eql(expectedModifiedSourceRecord);
+
+    function generateReducers(tagPattern, config) {
+
+
+      /*
+      if (tagPattern) {
+        return createReducer({tagPattern});
+      }
+      return createReducer();
+      */
+
+      return createReducer(tagPattern, config);
+    }
+  }
+});
