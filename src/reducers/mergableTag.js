f45c814638ac8c6bc2b6e934f55909997f125fc0
nvolk <nicholas.volk@helsinki.fi>
2022-08-30 15:51:27 +0300

    Add historical non-addable fields to non-mergable fields

--- src/reducers/mergableTag.js
+++ src/reducers/mergableTag.js
@@ -2,7 +2,11 @@
 //import {nvdebug} from './utils';
 //const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 
+// Actually I hope that 066. 842-845, 852-855. 863-868, 876-878 and HLI are already removed
+
+
 const defaultNonMergableFields = [
+  '066', // 066 - Character sets present (NR)
   '382', // 382: merging would be madness... However, this will miss cases, where only $5 or $9 differs...
   // 59X: always copy, never merge. NB! No specs exist!
   '590',
@@ -39,8 +43,26 @@ const defaultNonMergableFields = [
   '785',
   '786',
   '787',
-  // Them 8XX fields are holdingds related fields:
-  '856',
+  // Them 8XX fields are holdings related fields:
+  '841',
+  '842',
+  '843',
+  '844',
+  '845',
+  '852',
+  '853',
+  '854',
+  '855',
+  '856', // This is mergable but so risky, that let's just ignore these
+  '863',
+  '864',
+  '865',
+  '866',
+  '867',
+  '868',
+  '876',
+  '877',
+  '878',
   '881',
   '882',
   '883',
@@ -55,6 +77,7 @@ const defaultNonMergableFields = [
   '960',
   '995',
   'CAT',
+  'HLI',
   'LOW',
   'SID'
 ];
