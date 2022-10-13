dd3de41f09acab387d5a69767b77c2c0759b7ffb
nvolk <nicholas.volk@helsinki.fi>
2022-10-13 13:58:57 +0300

    Counterpart comparison always uses normalization (and possibly other changes)

--- src/reducers/counterpartField.js
+++ src/reducers/counterpartField.js
@@ -3,6 +3,7 @@
 import createDebugLogger from 'debug';
 import {fieldHasSubfield, fieldHasNSubfields, fieldToString, nvdebug} from './utils';
 import {cloneAndNormalizeField} from './normalize';
+// This should be done via our own normalizer:
 import {normalizeControlSubfieldValue} from '@natlibfi/marc-record-validators-melinda/dist/normalize-identifiers';
 
 import {getMergeConstraintsForTag} from './mergeConstraints';
@@ -15,6 +16,56 @@ const counterpartRegexps = {
   '100': /^[17]00$/u, '110': /^[17]10$/u, '111': /^[17]11$/u, '130': /^[17]30$/u,
   '700': /^[17]00$/u, '710': /^[17]10$/u, '711': /^[17]11$/u, '730': /^[17]30$/u
 };
+
+function pairableValue(tag, subfieldCode, value1, value2) {
+  // Return better value...
+  if (tag === '490') {
+    if (subfieldCode === 'v') {
+      // Stupid placeholder:
+      if (value1 === 'osa 1' && value2 === '1') {
+        return value1;
+      }
+      // We need a generic solution ("osa 1" equal "del I")
+    }
+  }
+
+  return undefined;
+}
+
+function optionalSubfieldComparison(originalBaseField, originalSourceField, keySubfieldsAsString) {
+  // We use clones here, since these changes done below are not intented to appear on the actual records.
+  const field1 = cloneAndNormalizeField(originalBaseField);
+  const field2 = cloneAndNormalizeField(originalSourceField);
+  if (keySubfieldsAsString === null) { // does not currently happen
+    // If keySubfieldsAsString is undefined, (practically) everything is the string.
+    // When everything is the string, the strings need to be (practically) identical.
+    // (NB! Here order matters. We should probably make it matter everywhere.)
+    // (However, keySubfieldsAsString === '' will always succeed. Used by 040 at least.)
+    return fieldToString(field1) === fieldToString(field2);
+  }
+  const subfieldArray = keySubfieldsAsString.split('');
+
+  return subfieldArray.every(subfieldCode => {
+    const subfieldValues1 = field1.subfields.filter(subfield => subfield.code === subfieldCode).map(sf => sf.value);
+    const subfieldValues2 = field2.subfields.filter(subfield => subfield.code === subfieldCode).map(sf => sf.value);
+    // If one side is empty, all is good
+    if (subfieldValues1.length === 0 || subfieldValues2.length === 0) {
+      return true;
+    }
+    // If one set is a subset of the other, all is good
+    if (subfieldValues1.every(val => subfieldValues2.includes(val)) || subfieldValues2.every(val => subfieldValues1.includes(val))) {
+      return true;
+    }
+
+    if (subfieldValues1.length === 1 && subfieldValues2.length === 1) {
+      return pairableValue(field1.tag, subfieldCode, subfieldValues1[0], subfieldValues2[0]) !== undefined;
+    }
+
+    return false;
+
+  });
+}
+
 
 function uniqueKeyMatches(baseField, sourceField, forcedKeyString = null) {
   // NB! Assume that field1 and field2 have same relevant subfields.
@@ -52,34 +103,6 @@ function mandatorySubfieldComparison(originalField1, originalField2, keySubfield
 
 }
 
-function optionalSubfieldComparison(originalBaseField, originalSourceField, keySubfieldsAsString) {
-  // We use clones here, since these changes done below are not intented to appear on the actual records.
-  const field1 = cloneAndNormalizeField(originalBaseField);
-  const field2 = cloneAndNormalizeField(originalSourceField);
-  if (keySubfieldsAsString === null) { // does not currently happen
-    // If keySubfieldsAsString is undefined, (practically) everything is the string.
-    // When everything is the string, the strings need to be (practically) identical.
-    // (NB! Here order matters. We should probably make it matter everywhere.)
-    // (However, keySubfieldsAsString === '' will always succeed. Used by 040 at least.)
-    return fieldToString(field1) === fieldToString(field2);
-  }
-  const subfieldArray = keySubfieldsAsString.split('');
-
-  return subfieldArray.every(subfieldCode => {
-    const subfieldValues1 = field1.subfields.filter(subfield => subfield.code === subfieldCode).map(sf => sf.value);
-    const subfieldValues2 = field2.subfields.filter(subfield => subfield.code === subfieldCode).map(sf => sf.value);
-    // If one side is empty, all is good
-    if (subfieldValues1.length === 0 || subfieldValues2.length === 0) {
-      return true;
-    }
-    // If one set is a subset of the other, all is good
-    if (subfieldValues1.every(val => subfieldValues2.includes(val)) || subfieldValues2.every(val => subfieldValues1.includes(val))) {
-      return true;
-    }
-    return false;
-
-  });
-}
 
 function tagToRegexp(tag) {
   if (tag in counterpartRegexps) {
@@ -128,13 +151,13 @@ function mergablePair(baseField, sourceField, config) {
     nvdebug('non-mergable (reason: control subfield)');
     return false;
   }
-  //debug('mergablePair()... wp2');
+
   // NB! field1.tag and field2.tag might differ (1XX vs 7XX). Therefore required subfields might theoretically differ as well. Thus check both:
   if (!areRequiredSubfieldsPresent(baseField) || !areRequiredSubfieldsPresent(sourceField)) {
     nvdebug('non-mergable (reason: missing subfields)');
     return false;
   }
-  //debug('mergablePair()... wp3');
+
   // Stuff of Hacks! Eg. require that both fields either have or have not X00$t:
   if (!arePairedSubfieldsInBalance(baseField, sourceField)) {
     nvdebug('required subfield pair check failed.');
@@ -186,30 +209,31 @@ function pairableName(baseField, sourceField) {
   // 100$a$t: remove $t and everything after that
   const reducedField1 = fieldToNamePart(baseField);
   const reducedField2 = fieldToNamePart(sourceField);
-
+  nvdebug(`IN: name mismatch: '${fieldToString(reducedField1)}' vs '${fieldToString(reducedField2)}'`);
   // Compare the remaining subsets...
   // First check that name matches...
   if (uniqueKeyMatches(reducedField1, reducedField2)) {
-    //debug(`    name match: '${fieldToString(reducedField1)}'`);
+    //nvdebug(`    name match: '${fieldToString(reducedField1)}'`);
     return true;
   }
 
 
   // However, mismatch is not critical! If Asteri ID matches, it's still a match!
+  // *NOT* sure whether this a good idea.
   if (pairableAsteriIDs(baseField, sourceField)) {
-    //debug(`    name match based on ASTERI $0'`);
+    //nvdebug(`    name match based on ASTERI $0'`);
     return true;
   }
 
 
-  //debug(`    name mismatch: '${fieldToString(reducedField1)}' vs '${fieldToString(reducedField2)}'`);
+  nvdebug(`    name mismatch: '${fieldToString(reducedField1)}' vs '${fieldToString(reducedField2)}'`);
   return false;
 }
 
 
 function semanticallyMergablePair(baseField, sourceField) {
-  // On rare occasions a field contains also a title part, name part and title part must
-  // be checked separately:
+  // On rare occasions a field contains also a title part. For these name part (= normally everything) and title part
+  // must be checked separately:
   if (!titlePartsMatch(baseField, sourceField)) {
     nvdebug(` ${baseField.tag} is unmergable: Title part mismatch.`);
     return false;
@@ -302,9 +326,13 @@ export function getCounterpart(record, field, config) {
 
   nvdebug(`Compare incoming '${fieldToString(field)}' with (up to) ${counterpartCands.length} existing field(s)`, debug);
 
+  const normalizedField = cloneAndNormalizeField(field);
+  nvdebug(` S: ${fieldToString(normalizedField)}`);
   // Then find (the index of) the first mathing candidate field and return it.
   const index = counterpartCands.findIndex((currCand) => {
-    if (mergablePair(currCand, field, config)) {
+    const normalizedCurrCand = cloneAndNormalizeField(currCand);
+    nvdebug(` B: ${fieldToString(normalizedCurrCand)}`);
+    if (mergablePair(normalizedCurrCand, normalizedField, config)) {
       nvdebug(`  OK pair found: '${fieldToString(currCand)}'. Returning it!`);
       return true;
     }
