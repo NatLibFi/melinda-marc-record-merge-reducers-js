2e6d0edc0913458e7810d8ca7249c8ddd30a3a5b
nvolk <nicholas.volk@helsinki.fi>
2022-10-26 13:05:44 +0300

    Add indicator fix for base

--- src/reducers/postprocessor.js
+++ src/reducers/postprocessor.js
@@ -1,10 +1,11 @@
-
 import fs from 'fs';
 import path from 'path';
 //import {MarcRecord} from '@natlibfi/marc-record';
 import {/*fieldRenameSubfieldCodes, fieldToString,*/ nvdebug /*recordReplaceField, stringToRegex*/} from './utils.js';
 import {filterOperations} from './preprocessor.js';
 import {deleteWorsePrepublicationFields500, deleteWorsePrepublicationFields594} from './preprocessPrepublication.js';
+
+import {recordNormalizeIndicators} from '@natlibfi/marc-record-validators-melinda/dist/indicator-fixes';
 
 const defaultConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'reducers', 'config.json'), 'utf8'));
 
@@ -19,6 +20,7 @@ export default (config = defaultConfig) => (base, source) => {
   filterOperations(base, source, config.postprocessorDirectives); // declared in preprocessor
   deleteWorsePrepublicationFields500(base);
   deleteWorsePrepublicationFields594(base);
+  recordNormalizeIndicators(base); // Fix 245 and non-filing indicators
   return {base, source};
 };
 
