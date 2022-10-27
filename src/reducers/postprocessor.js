529a9c8386f3ab69b390033ce9a373af97d2518a
nvolk <nicholas.volk@helsinki.fi>
2022-10-27 12:55:20 +0300

    Postporcessing: call deleteAllPrepublicationNotesFromField500(base). Reduce logging.

--- src/reducers/postprocessor.js
+++ src/reducers/postprocessor.js
@@ -6,18 +6,21 @@ import {filterOperations} from './preprocessor.js';
 import {deleteWorsePrepublicationFields500, deleteWorsePrepublicationFields594} from './preprocessPrepublication.js';
 
 import {recordNormalizeIndicators} from '@natlibfi/marc-record-validators-melinda/dist/indicator-fixes';
+import {deleteAllPrepublicationNotesFromField500} from './prepublicationUtils.js';
 
 const defaultConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'reducers', 'config.json'), 'utf8'));
 
 export default (config = defaultConfig) => (base, source) => {
-  nvdebug('ENTERIN preprocessor.js');
-  nvdebug(JSON.stringify(base));
-  nvdebug(JSON.stringify(source));
+  nvdebug('ENTERING postprocessor.js');
+  //nvdebug(JSON.stringify(base));
+  //nvdebug(JSON.stringify(source));
 
-  nvdebug(JSON.stringify(config.postprocessorDirectives));
+  //nvdebug(JSON.stringify(config.postprocessorDirectives));
   //const baseRecord = new MarcRecord(base, {subfieldValues: false});
   //nvdebug(`HSP CONF ${config}`);
   filterOperations(base, source, config.postprocessorDirectives); // declared in preprocessor
+
+  deleteAllPrepublicationNotesFromField500(base);
   deleteWorsePrepublicationFields500(base);
   deleteWorsePrepublicationFields594(base);
   recordNormalizeIndicators(base); // Fix 245 and non-filing indicators
