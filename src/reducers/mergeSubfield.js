6ead1859a480d637fc75b98dd135c2bf6579b3f6
nvolk <nicholas.volk@helsinki.fi>
2022-11-01 22:48:44 +0200

    Implement 020$q mappings from binding to cover type

--- src/reducers/mergeSubfield.js
+++ src/reducers/mergeSubfield.js
@@ -69,6 +69,85 @@ function replaceDatesAssociatedWithName(targetField, candSubfield, relevantSubfi
   return false;
 }
 
+// use array.includes(value) for easy extendability (Swedish, other languages, abbrs, etc.()
+function isKierreselka(value) {
+  return ['kierreselkä'].includes(value);
+}
+
+function isKovakantinen(value) {
+  return ['kovakantinen'].includes(value);
+}
+
+function isPehmeakantinen(value) {
+  return ['pehmeäkantinen'].includes(value);
+}
+
+function isRengaskirja(value) {
+  return ['rengaskirja'].includes(value);
+}
+
+function isSidottu(value) {
+  return ['sid.', 'sidottu'].includes(value);
+}
+
+function isNidottu(value) {
+  return ['nid.', 'nidottu'].includes(value);
+}
+
+
+function pairKovakantinen(candidateSubfields) {
+  return candidateSubfields.find(sf => sf.code === 'q' && isSidottu(sf.value));
+}
+
+function pairPehmeakantinen(candidateSubfields) {
+  return candidateSubfields.find(sf => sf.code === 'q' && isNidottu(sf.value));
+}
+
+function pairKierreselka(candidateSubfields) {
+  return candidateSubfields.find(sf => sf.code === 'q' && isRengaskirja(sf.value));
+}
+
+
+function findCounterpartForCoverType(coverType, candidateSubfields) {
+  if (isKovakantinen(coverType)) {
+    return pairKovakantinen(candidateSubfields);
+  }
+  if (isPehmeakantinen(coverType)) {
+    return pairPehmeakantinen(candidateSubfields);
+  }
+  if (isKierreselka(coverType)) {
+    return pairKierreselka(candidateSubfields);
+  }
+  return null;
+}
+
+
+function mapBindingToCoverType(field, candSubfield, relevantSubfields) {
+  if (field.tag !== '020' || candSubfield.code !== 'q') {
+    return false;
+  }
+  // See if base's field has a binding and source subfield has a cover. If so, replace subfield value:
+  const originalSubfieldQ = findCounterpartForCoverType(candSubfield.value, relevantSubfields);
+  if (originalSubfieldQ) {
+    originalSubfieldQ.value = candSubfield.value; // eslint-disable-line functional/immutable-data
+    return true;
+  }
+  // Vice versa: if original value is cover type nad new value is ye olde binding, return true.
+  // Then change is considered to have happenened.
+  if (isNidottu(candSubfield.value) && relevantSubfields.some(sf => isPehmeakantinen(sf.value))) {
+    return true;
+  }
+  if (isSidottu(candSubfield.value) && relevantSubfields.some(sf => isKovakantinen(sf.value))) {
+    return true;
+  }
+  if (isRengaskirja(candSubfield.value) && relevantSubfields.some(sf => isKierreselka(sf.value))) {
+    return true;
+  }
+
+
+  return false;
+}
+
 export function mergeSubfield(targetField, candSubfield) {
   // Replace existing subfield with the incoming field. These replacements are by name rather hacky...
   // Currenty we only select the better X00$d.
@@ -79,15 +158,18 @@ export function mergeSubfield(targetField, candSubfield) {
   // Return true, if replace is done.
   // However, replacing/succeeding requires a sanity check, that the new value is a better one...
   // Thus, typically this function fails...
+
   const relevantSubfields = targetField.subfields.filter(subfield => subfield.code === candSubfield.code);
   if (relevantSubfields.length === 0) { // There must be something that gets replaced.
     return false;
   }
   debug(`Got ${relevantSubfields.length} sf-cands for field ${targetField.tag}‡${candSubfield.code}`);
 
-  if (replaceDatesAssociatedWithName(targetField, candSubfield, relevantSubfields)) {
+  if (replaceDatesAssociatedWithName(targetField, candSubfield, relevantSubfields) ||
+      mapBindingToCoverType(targetField, candSubfield, relevantSubfields)) {
     return true;
   }
+
 
   // Mark 490$v "osa 1" vs "1" as merged (2nd part of MET-53).
   // NB! Keeps the original value and drops the incoming value. (Just preventing it from going to add-part...)
