b968cb9541b024ea175e11b02873da428f5f433d
nvolk <nicholas.volk@helsinki.fi>
2022-11-23 23:52:33 +0200

    Add $6-two-field-duplicate removals (fixes MRA-156). Allow 3+ digit indexes in $6 TAG-INDEX.

--- src/reducers/postprocessSubfield6.js
+++ src/reducers/postprocessSubfield6.js
@@ -1,24 +1,27 @@
-//import createDebugLogger from 'debug';
-import {MarcRecord} from '@natlibfi/marc-record';
-import {/*fieldToString, fieldHasNSubfields,*/ fieldHasSubfield /*, nvdebug*/} from './utils';
+import createDebugLogger from 'debug';
+import {fieldHasSubfield, fieldToString, nvdebug} from './utils';
 
-//const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
+const debug = createDebugLogger('@natlibfi/melinda-marc-record-merge-reducers');
 //const debugData = debug.extend('data');
 
-
-const sf6Regexp = /^[0-9][0-9][0-9]-[0-9][0-9]/u;
+// CHECK: what if index is greater than 99?
+const sf6Regexp = /^[0-9][0-9][0-9]-[0-9][0-9]+/u;
 
 // Remove unpaired
 export default () => (base, source) => {
   //nvdebug(`ENTERING postprocessSubfield6.js`);
   //nvdebug(JSON.stringify(base));
   //nvdebug(JSON.stringify(source));
-  const baseRecord = new MarcRecord(base, {subfieldValues: false});
-  recordRemovePairlessFields(baseRecord, false);
-  return {base: baseRecord, source};
+  //const baseRecord = new MarcRecord(base, {subfieldValues: false});
+  recordRemovePairlessFields(base, false); // Or should pairless 880 $6 520-05 become $6 520-00 etc?
+  removeDuplicateFieldPairs(base);
+  return {base, source};
 };
 
 function get6s(field) {
+  if (!field.subfields) {
+    return [];
+  }
   return field.subfields.filter(sf => sf.code === '6');
 }
 
@@ -33,11 +36,14 @@ function subfieldApplies(subfield, lookFor) {
   if (subfield.code !== '6') {
     return false;
   }
-  return subfield.value.substring(0, 6) === lookFor;
+  const key = subfield.value.replace(/^([0-9][0-9][0-9]-[0-9][0-9]+).*$/u, '$1'); // eslint-disable-line prefer-named-capture-group
+  nvdebug(` Compare '${key}' vs '${lookFor}'`);
+  return key === lookFor;
 }
 
 function getPairValue(subfield6, myTag) {
-  const index = subfield6.value.substring(4, 6);
+  const index = subfield6.value.replace(/^[0-9][0-9][0-9]-([0-9][0-9]+).*$/u, '$1'); // eslint-disable-line prefer-named-capture-group
+
   const lookFor = `${myTag}-${index}`;
   return lookFor;
 }
@@ -47,10 +53,13 @@ function findPairForSubfield6(subfield6, myTag, fields) {
   if (!subfield6.value.match(sf6Regexp)) {
     return undefined;
   }
+
   // Only valid $6 value that fails to map to another field is iffy...
   const referredTag = subfield6.value.substring(0, 3);
+
   //const index = subfield6.value.substring(4, 6);
   const lookFor = getPairValue(subfield6, myTag);
+  nvdebug(`Try to find  ${lookFor}...`);
   const relevantFields = fields.filter(field => field.tag === referredTag && field.subfields.some(sf => subfieldApplies(sf, lookFor)));
   if (relevantFields.length === 0) {
     return undefined;
@@ -59,45 +68,6 @@ function findPairForSubfield6(subfield6, myTag, fields) {
   return relevantFields[0];
 }
 
-/*
-export function resetCorrespondingField880(field, record, oldTag, newTag) {
-  const sixes = get6s(field);
-  if (sixes.length === 0) { // speed things up
-    return;
-  }
-  const cand880Fields = record.fields.filter(field => field.tag === '880');
-  sixes.forEach(sf6 => fix880(sf6));
-
-  function fix880(sf6) {
-    const pairValue = getPairValue(sf6, oldTag);
-    const newPairValue = `${newTag}-${pairValue.substring(4, 6)}`;
-    cand880Fields.forEach(f => f.subfields.forEach(sf => fix880Subfield6(sf, pairValue, newPairValue)));
-  }
-
-  function fix880Subfield6(sf, oldValue, newValue) {
-    if (sf.code === '6' && sf.value === oldValue) {
-      sf.value = newValue; // eslint-disable-line functional/immutable-data
-      return;
-    }
-  }
-}
-*/
-
-/*
-function subfield6HasNoPair(subfield6, ownTag, fields) {
-  // We keep the crap!
-  if (!subfield6.value.match(sf6Regexp)) {
-    return true; // huh!?!, had function name there: fieldHasNSubfields;
-  }
-  // Only valid $6 value that fails to map to another field is iffy...
-  const referredTag = subfield6.value.substring(0, 3);
-  const index = subfield6.value.substring(4, 6);
-  const lookFor = `${ownTag}-${index}`;
-
-  const relevantFields = fields.filter(field => field.tag === referredTag && field.subfields.some(sf => subfieldApplies(sf, lookFor)));
-  return relevantFields.length === 0; // Valid pair not found
-}
-*/
 
 function cleanAndReturnTrueIfDeletable(field, fields) {
   const sixes = get6s(field);
@@ -118,9 +88,57 @@ function cleanAndReturnTrueIfDeletable(field, fields) {
 }
 
 
-export function recordRemovePairlessFields(record, removedOnlyMerged) {
+function get6lessClone(field) {
+  return {
+    'tag': field.tag,
+    'ind1': field.ind1,
+    'ind2': field.ind2,
+    'subfields': field.subfields.filter(sf => sf.code !== '6')
+  };
+}
+
+
+export function removeDuplicateFieldPairs(record) { // Try to fix MRA-156
+  /* eslint-disable */
+  let seen = {};
+
+  record.fields.forEach(field => processField(field));
+
+  function processField(field) {
+    if (field.tag === '880') {
+      return;
+    }
+
+    const sixes = get6s(field);
+    if ( sixes.length !== 1) {
+      return;
+    }
+
+    nvdebug(`Try to pair ${fieldToString(field)}`, debug);
+    const pairField = findPairForSubfield6(sixes[0], field.tag, record.fields);
+    if (!pairField) {
+      nvdebug(` No pair for ${fieldToString(field)}`, debug);
+      return;
+    }
+    const fieldString = fieldToString(get6lessClone(field));
+    const pairFieldString = fieldToString(get6lessClone(pairField));
+    if (seen[fieldString]) {
+      if (seen[fieldString] === pairFieldString) {
+        nvdebug(`Remove '${fieldString}' and '${pairFieldString}`, debug);
+        record.removeField(field);
+        record.removeField(pairField);
+        return;
+      }
+      return;
+    }
+    seen[fieldString] = pairFieldString;
+  }
+  /* eslint-enable */
+}
+
+export function recordRemovePairlessFields(record) {
   const relevantFields = record.fields.filter(field => fieldHasSubfield(field, '6'));
-  const deletableFields = relevantFields.filter(field => cleanAndReturnTrueIfDeletable(field, relevantFields, removedOnlyMerged));
+  const deletableFields = relevantFields.filter(field => cleanAndReturnTrueIfDeletable(field, relevantFields));
 
   deletableFields.forEach(field => record.removeField(field));
 
