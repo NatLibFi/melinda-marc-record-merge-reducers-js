16b87e5d0d0d17f673c36652ec39f4be97a3ecc8
nvolk <nicholas.volk@helsinki.fi>
2022-11-23 14:24:39 +0200

    Allow some variation about print (p./painos/uppl.) and year (yyyy/[yyyy])

--- src/reducers/mergeField500Lisapainokset.js
+++ src/reducers/mergeField500Lisapainokset.js
@@ -17,11 +17,64 @@ function validLisapainosField(field) {
   if (field.tag !== '500' || field.subfields.length !== 1 || field.subfields[0].code !== 'a') {
     return false;
   }
-  return field.subfields[0].value.match(/^(?:Lisäpainokset|Lisäpainos): (?:[1-9][0-9]*\. p\. [0-9]+\.)(?: - [1-9][0-9]*\. p\. [0-9]+\.)*$/u);
+  return field.subfields[0].value.match(/^(?:Lisäpainokset|Lisäpainos): (?:[1-9][0-9]*\. (?:p\.|painos|uppl\.) [0-9]+\.)(?: - [1-9][0-9]*\. (?:p\.|painos|uppl\.) \[?[0-9]+\]?\.)*$/u);
 }
 
 function fieldToPrintsString(field) {
+  // Could this just be something on the lines of s/^\S+ // ?
   return field.subfields[0].value.replace(/^(?:Lisäpainokset|Lisäpainos): /u, '').replace(/\.$/u, '');
+}
+
+const printPreference = ['painos', 'p.', 'upplaga', 'uppl.'];
+function getPrintPreference(value) {
+  return printPreference.findIndex(pp => pp === value);
+}
+
+function mergePrintData(value1, value2) {
+  const [index1, print1, year1] = value1.split(' ');
+  const [index2, print2, year2] = value2.split(' ');
+
+  const betterIndex = index1 ? index1 : index2; // just to cheat eslint...
+
+  // merge print1 and print2
+  const betterPrint = getBetterPrint(print1, print2);
+  if (!betterPrint) {
+    return null;
+  }
+
+  const betterYear = getBetterYear(year1, year2);
+  if (!betterYear) {
+    return null;
+  }
+
+  return `${betterIndex} ${betterPrint} ${betterYear}`;
+
+  function getBetterYear(y1, y2) {
+    if (y1 === y2 || y2 === `[${y1}]`) {
+      return y1;
+    }
+    if (y1 === `[${y2}]`) {
+      return y2;
+    }
+    return null;
+  }
+
+  function getBetterPrint(print1, print2) {
+    if (print1 === print2) {
+      return print1;
+    }
+
+    const i1 = getPrintPreference(print1);
+    const i2 = getPrintPreference(print2);
+    if (i1 === -1 || i2 === -1) {
+      return null;
+    }
+    if (i1 <= i2) {
+      return printPreference[i1];
+    }
+    return printPreference[i2];
+  }
+
 }
 
 export function mergeLisapainokset(record) {
@@ -39,13 +92,17 @@ export function mergeLisapainokset(record) {
     const value = fieldToPrintsString(relevantFields[i]);
     const fieldsPrintData = value.split('. - ');
     for (j=0; j < fieldsPrintData.length; j++) {
-      const currPrintData = fieldsPrintData[j];
+      let currPrintData = fieldsPrintData[j];
       // Example value: "2. p. 2020"
       const [ printIndex ] = currPrintData.split('.');
       if (allPrintData[printIndex] !== undefined) {
-        if (allPrintData[printIndex] !== currPrintData) {
-          nvdebug(`MISMATCH:\n '${currPrintData}'\n '${allPrintData[printIndex]}'`, debug);
-          return; // reason for for-loops: exit function from within nested loops
+        if (allPrintData[printIndex] !== currPrintData ) {
+          const mergedPrintData = mergePrintData(allPrintData[printIndex], currPrintData);
+          if (!mergedPrintData) {
+            nvdebug(`MISMATCH:\n '${currPrintData}'\n '${allPrintData[printIndex]}'`, debug);
+            return; // reason for for-loops: exit function from within nested loops
+          }
+          currPrintData = mergedPrintData;
         }
       }
       allPrintData[printIndex] = currPrintData;
