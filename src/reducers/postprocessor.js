da3861d4e6f554fcb4b166c03bb0b27621d6565e
nvolk <nicholas.volk@helsinki.fi>
2023-03-10 00:30:24 +0200

    Use external $0 clean-up (MRA-392) and (currently buggy) duplicate field removal.

--- src/reducers/postprocessor.js
+++ src/reducers/postprocessor.js
@@ -3,13 +3,16 @@ import path from 'path';
 
 import {nvdebug} from './utils.js';
 import {filterOperations} from './processFilter.js';
-import {removeDuplicateDatafields} from './removeIdenticalDataFields';
+//import {removeDuplicateDatafields as removeDuplicateDatafieldsOld} from './removeIdenticalDataFields';
 
 import {recordNormalizeIndicators} from '@natlibfi/marc-record-validators-melinda/dist/indicator-fixes';
 import {deleteAllPrepublicationNotesFromField500InNonPubRecord, removeWorsePrepubField500s, removeWorsePrepubField594s} from './prepublicationUtils.js';
 import {mergeLisapainokset} from '@natlibfi/marc-record-validators-melinda/dist/mergeField500Lisapainokset';
 import {recordResetSubfield6OccurrenceNumbers} from '@natlibfi/marc-record-validators-melinda/dist/reindexSubfield6OccurenceNumbers';
 
+import {removeDuplicateDatafields} from '@natlibfi/marc-record-validators-melinda/dist/removeDuplicateDataFields';
+
+import factoryForThereCanBeOnlyOneSubfield0 from '@natlibfi/marc-record-validators-melinda/dist/multiple-subfield-0';
 const defaultConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'src', 'reducers', 'config.json'), 'utf8'));
 
 export default (config = defaultConfig) => (base, source) => {
@@ -34,8 +37,16 @@ export default (config = defaultConfig) => (base, source) => {
   mergeLisapainokset(base);
   //base.fields.forEach(field => nvdebug(`WP7: ${fieldToString(field)}`));
 
+  const thereCanBeOnlyOneSubfield0 = factoryForThereCanBeOnlyOneSubfield0({});
+  thereCanBeOnlyOneSubfield0.fix(base);
 
-  removeDuplicateDatafields(base);
+  const res = removeDuplicateDatafields(base, true); // NB! 10.2 is buggy!
+  nvdebug(`Re-DUP ${JSON.stringify(res)}`);
+
+  //res.message.forEach(msg => nvdebug(msg));
+
+  //removeDuplicateDatafieldsOld(base);
+
   recordResetSubfield6OccurrenceNumbers(base);
   return {base, source};
 };
