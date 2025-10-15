d2a7e5831b3ac67566b29d7d39b2487e2cb58b13
Minttu Hurme <ammsalme@users.noreply.github.com>
2025-10-15 11:57:31 +0300

    Add reducer set for Merge UI

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -44,6 +44,29 @@ export const localReducers = [
   postprocessor()
 ];
 
+export const mergeReducersForMergeUI = [
+  //// PREPROCESSOR STUFF:
+  // UTF-8 normalization: if wanted, see mergeField.js for an example
+  genericPreprocessor(), // Should this be moved downward?
+  metatietosanastoNormalizations(),
+  prepublicationPreprocessor(),
+  removeDuplicatesFromSource(), // handles $6 and $8 chains as well (but apparently badly)
+  reindexSubfield6(), // Reindex $6 subfields from source, base remains unchanged.
+  reindexSubfield8(), // Reindex $8 subfields from source, base remains unchanged.
+  manufacturer260To264(),
+
+  //// ACTUAL MERGE/ADD STUFF:
+  //internalFields(), // LOW, CAT, SID. Nowadays part of genericDatafield()
+  leader(), // Test 01
+  field006(), // Tests 02 and 03
+  field007(), // Tests 04 and 05
+  field008(), // Tests 06, 07, and 08
+  mergeDataFields(),
+  addDataFields(),
+  postprocessor()
+];
+
+
 export const muuntajaReducers = [
   genericPreprocessor(),
   metatietosanastoNormalizations(),
